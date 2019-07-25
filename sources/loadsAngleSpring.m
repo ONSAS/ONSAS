5abaeb429adb686df83bc8034e0dc284c880da64
Jorge Perez Zerpa <jorgepz@fing.edu.uy>
2019-07-25 00:29:31 -0300

    h:01.50 working on angle springs

--- sources/loadsAngleSpring.m
+++ sources/loadsAngleSpring.m
@@ -1,33 +1,36 @@
 clear all, close all
 
-%~ function fintAngSpr
-n=40;
+%~ n  = 40 ;
+%~ ts = linspace(0,pi,n)' ;
 
-ts = linspace(0,pi,n)' ;
+%~ indselim = unique( round( rand(round(0.5*n),1)*(n*.9) + 1 ) )
+%ts (indselim) = [];
+%~ n = length(ts);
 
-indselim = unique( round( rand(round(0.5*n),1)*(n*.95) + 1 ) )
-%~ ts (indselim) = [];
+%~ Rx = 3; Ry = 3;
+%~ xs = Rx*cos(ts);
+%~ ys = Ry*sin(ts);
+%~ zs = ts*0;
 
 
-Rx = 4;
-Ry = 2;
-%~ xs = linspace(0,1,n)';
-%~ ys = xs.^2 ;
-xs = Rx*cos(ts);
-ys = Ry*sin(ts);
-zs = ts*0;
+xs = [  0 1 2 3]' ;
+ys = [  0 1 1 0]' ;
+zs = 0 * xs ;
 
+%~ xs = [  0 1 2 3]' ;
+%~ ys = [  0 1 0 1]' ;
+%~ zs = 0 * xs ;
+
+n = length(xs);
 
 EI  =1 ;
 
 Nodes = [ xs ys zeros(size(ys)) ];
 
-n=size(Nodes,1);
+Conec = [ (1:(n-1))' (2:(n))' ];
 
 fextAngSpr = zeros( 6*n,1);
-
           
-Conec = [ (1:(n-1))' (2:(n))' ];
 
 chis = zeros( size(Nodes,1) ,1 );          
 
@@ -36,38 +39,38 @@ for i=1 : (size(Conec,1) -1 )
   indxi   = Conec(i  ,2) ;
   indxip1 = Conec(i+1,2) ;
   
-  ti   = Nodes(indxi  ,:) - Nodes(indxim1,:) ;
-  tip1 = Nodes(indxip1,:) - Nodes(indxi  ,:) ;
+  ti   = ( Nodes(indxi  ,:) - Nodes(indxim1,:) )'
+  tip1 = ( Nodes(indxip1,:) - Nodes(indxi  ,:) )'
 
-  li   = norm( ti);
-  lip1 = norm( tip1);
+  li   = norm( ti   )
+  lip1 = norm( tip1 )
   
-  dtds = ( tip1/lip1 - ti/li ) / ( lip1/2 + li/2 ) ;
-  chis(i+1) = norm( dtds );
+  dtds = ( tip1/lip1 - ti/li ) / ( lip1/2 + li/2 ) 
+  chis(i+1) = norm( dtds )
   %~ normal = dtds / chis(i+1) ;
 
-  Fiim1 = -dtds *  EI * chis(i+1) / norm( cross( ti  , dtds) )  ;
-  Fim1i =  dtds *  EI * chis(i+1) / norm( cross( ti  , dtds) )  ;
+  Fiim1 = -dtds *  EI * chis(i+1) / norm( cross( ti  , dtds) )  
+  Fim1i =  dtds *  EI * chis(i+1) / norm( cross( ti  , dtds) )  
   
-  Fiip1 = -dtds *  EI * chis(i+1) / norm( cross( tip1, dtds) )  ;
-  Fip1i =  dtds *  EI * chis(i+1) / norm( cross( tip1, dtds) )  ;
+  Fiip1 = -dtds *  EI * chis(i+1) / norm( cross( tip1, dtds) )  
+  Fip1i =  dtds *  EI * chis(i+1) / norm( cross( tip1, dtds) )  
 
   aux = nodes2dofs( indxim1,6) ;
-  fextAngSpr( aux(1:2:6) ) = Fim1i ; 
+  fextAngSpr( aux(1:2:6) ) = fextAngSpr( aux(1:2:6) ) + Fim1i ; 
 
   aux = nodes2dofs( indxi  ,6) ;
-  fextAngSpr( aux(1:2:6) ) = Fiim1 + Fiip1 ; 
+  fextAngSpr( aux(1:2:6) ) = fextAngSpr( aux(1:2:6) ) + Fiim1 + Fiip1 ; 
 
   aux = nodes2dofs( indxip1,6) ;
-  fextAngSpr( aux(1:2:6) ) = Fip1i ; 
+  fextAngSpr( aux(1:2:6) ) = fextAngSpr( aux(1:2:6) ) + Fip1i ; 
   
   %~ pause
 end
 
 
 
-1/ ( Rx^2 / Ry )
-1/ ( Ry^2 / Rx )
+%~ 1/ ( Rx^2 / Ry )
+%~ 1/ ( Ry^2 / Rx )
 
 
 figure
@@ -75,11 +78,11 @@ figure
 plot3(xs,ys,zs,'o-g')
 hold on
 %~ quiver(xs,ys,zs,fextAngSpr(1:6:end),fextAngSpr(3:6:end),fextAngSpr(3:6:end),'o-r')
-quiver3(xs,ys,zs,fextAngSpr(1:6:end),fextAngSpr(3:6:end),fextAngSpr(3:6:end),'o-r')
+quiver3(xs,ys,zs,fextAngSpr(1:6:end),fextAngSpr(3:6:end),fextAngSpr(5:6:end),'o-r')
 axis equal
 
-figure
-plot(ts,chis,'x-r')
+%~ figure
+%~ plot(ts,chis,'x-r')
 %~ Nodes
 
 
