de06cdb586f9e2e87adea3be238efa97233b7786
Jorge <jorgepz@fing.edu.uy>
2019-08-16 08:56:27 -0300

    change in change directory command in mshReader

--- runTestProblems.m
+++ runTestProblems.m
@@ -9,8 +9,8 @@ for previouslyDefinedSelectedFileVar = 1:length(readdir('./input'))
     ONSAS
   
     if noErrorsOccurred == 1
-      fprintf('No errors occurred. Press any key to continue...');
-      pause,
+      fprintf('No errors occurred. Waiting 2 seconds...');
+      pause(2),
       fprintf('\n\n'); clear all, close all
     else
       error('Errors occurred');
