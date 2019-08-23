9676c9acff349ac0645586fe75a2f828c0b50f0f
Joaquin Viera <joaquinv@fing.edu.uy>
2019-08-23 08:26:18 -0300

    h:00.10 Linear analysis example added. ONSAS has differences with the analytical solution.

---
+++ input/R2_pr3_ej3.m
@@ -0,0 +1,80 @@
+% --------------------------------------------------------------------------------------------------
+% Test problem: Practico 3 ej 1 R2.
+% --------------------------------------------------------------------------------------------------
+
+E  = 210e6   ;
+nu = 0.3    ;
+Iy = 1450 / (100^4) ;
+A  = sqrt( Iy )*1e9 ; 
+%~ A	 = 27.9 / (100^2) ;
+
+qz = -15 ;
+H = 15 ;
+
+inputONSASversion = '0.1.8';
+
+problemName = 'R2_pr3_ej3' ;
+
+% constitutive properties
+
+hyperElasParams = cell(1,1) ;
+hyperElasParams{1} = [1 E nu] ;
+
+% geometrical properties
+
+secGeomProps = [ A Iy 1 1 ] ;
+
+Nodes = [ 0 0 0 ; ...
+					0 0 2.4 ; ...
+					0 0 4.8 ; ...
+					4 0 4.8 ; ...
+					5.2 0 4.8 ; ...
+					4 0 0 ] ;
+
+% in global system of coordinates
+nodalSprings = [ 1  	inf  inf  inf  inf  	inf  inf ; ...
+                 6  	inf  inf  inf  	 0  	inf  inf ] ;
+
+Conec = [ 1 2 0 0 1 1 2 ; ...
+          2 3 0 0 1 1 2 ; ...
+          3 4 0 0 1 1 2 ; ...
+          4 5 0 0 1 1 2 ; ...
+          4 6 0 0 1 1 2 ; ...
+          1 4 0 0 1 1 1 ] ;
+          
+%~ elemReleases = [ 0 0 0 0 ; ...
+								 %~ 0 0 0 0 ; ... 
+								 %~ 0 0 0 0 ; ...
+								 %~ 0 0 0 0 ; ...
+								 %~ 0 0 0 0 ; ...
+								 %~ 1 1 1 1 ]           
+
+%~ unifLoad = [ 3 1 0 0 qz ] ;
+
+M = -qz*1.2^2/2 ;
+V = qz*1.2 ;
+
+
+nodalConstantLoads = [ 2 H 0 0 0 0 0 ; ...
+											 3 0 0 0 -M V 0 ; ...
+											 4 0 0 0 2*M V 0 ] ; 
+
+% analysis parameters
+nonLinearAnalysisBoolean = 0 ; 
+dynamicAnalysisBoolean   = 0 ; 
+LBAAnalyFlag             = 0 ;
+
+% [ node nodaldof scalefactor(positive or negative) ]
+controlDofInfo = [ 1 1 1 ] ;
+
+
+printflag = 0 ;
+tablesBoolean = 1;
+
+plotParamsVector = [2];
+
+plotsViewAxis = [ 0 -1 0] ;
+
+linearDeformedScaleFactor = 1 ;  
+
+% --------------------------------------------------------------------------------------------------
