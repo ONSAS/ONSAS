80df4f4fbf84fe4a4788a4f0e5414e341b6e14a3
Jorge Pérez Zerpa <jorgepz@fing.edu.uy>
2020-05-13 11:39:01 -0300

    separated function for Boundary Conditions dofs vectors generation

--- sources/initialDefinitions.m
+++ sources/initialDefinitions.m
@@ -22,50 +22,7 @@
 tic ;
 
 % ----------- fixeddofs and spring matrix computation ---------
-fixeddofs = [] ;
-KS      = sparse( 6*nnodes, 6*nnodes );  
-
-for i=1:size(nodalSprings,1)
-  aux = nodes2dofs ( nodalSprings (i,1) , 6 ) ;
-  for k=1:6
-    %
-    if nodalSprings(i,k+1) == inf,
-      fixeddofs = [ fixeddofs; aux(k) ] ;
-
-    elseif nodalSprings(i,k+1) > 0,
-      KS( aux(k), aux(k) ) = KS( aux(k), aux(k) ) + nodalSprings(i,k+1) ;
-
-    end
-  end
-end
-
-diridofs = fixeddofs ;
-diridofs = unique( diridofs) ; % remove repeated dofs
-%~ diridofs = [ diridofs ; releasesDofs] ;
-
-neumdofs = zeros( 6*nnodes, 1 ) ;
-
-for elem = 1:nelems
-  
-  aux = nodes2dofs( Conec( elem, 1:4), 6)' ;
-  
-  switch Conec( elem, 7)
-  case 1
-    neumdofs ( aux(1:2:11) ) = aux(1:2:11) ;
-  case 2
-    neumdofs ( aux(1:11) ) = aux(1:11) ;
-  case 3
-    neumdofs ( aux(1:2:(6*4-1) ) ) = aux(1:2:(6*4-1)) ;
-  end  
-end
-
-neumdofs( diridofs ) = 0 ;
-
-neumdofs = unique( neumdofs ) ;
-if neumdofs(1) == 0,
-  neumdofs(1)=[];
-end
-% -------------------------------------------------------------
+[neumdofs, diridofs, KS] = computeBCDofs(nnodes, Conec, nelems, nodalSprings )
 
 
 loadFactors     = 0 ;
