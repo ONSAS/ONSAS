fb56986cf4f8d5620a2f94752b11348848e28cdd
Jorge <jorgepz@fing.edu.uy>
2020-08-30 12:36:08 -0300

    improve heat conduction auxiliar script

--- examples/aux_toyHeatTransferExample.m
+++ examples/aux_toyHeatTransferExample.m
@@ -6,78 +6,101 @@ clear all, close all
 
 addpath('../sources/')
 
-nelem  = 100 ;
-Tfinal = 1 ;
-dt     = .0001 ;
-Area   = 1 ;
+nelem  = 10 ;
+Tfinal = .5 ;
+dt     = .001 ;
 rho    = 1 ;
 cSpHe  = 1 ;
 kCond  = 1 ;
-Ltot   = 1 ;
+Ltot   = 1 ; % domain [0,1]
+Area   = 1 ;
 
-%~ nt     = Tfinal / dt ;
-nt     = 10 ;
+%~ nt     = 3 ; % defined if stopped before Tfinal
+
+if exist('nt')==0
+  nt     = Tfinal / dt ;
+end
+
+alpha = kCond / ( rho * cSpHe ) ;
 
 nnodes = nelem +1 ;
 
-xs = linspace(0,Ltot,nnodes )' ;
-T0     = sin(pi*xs) + 0.5*sin(3*pi*xs) ;
+nnodesAnly = 100 ;
+
+xs     = linspace(0, Ltot, nnodes     )' ;
+xsAnly = linspace(0, Ltot, nnodesAnly )' ;
 
 diridofs = [ 1 nnodes ] ;
 Tdiri  = 0 ;
 
 neumdofs = 1:nnodes ;
-
 neumdofs( diridofs ) = [] ;
-
 
 lelem  = Ltot/nelem ;
 
+% local elemental diffussion equation
 Kdiffe = kCond * Area / lelem * [ 1 -1 ; -1 1 ] ;
 
 MintEe = rho * cSpHe * Area * lelem / 6 * [ 2 1 ; 1 2 ] ;
 
+% initial temperature
+T0     = sin(pi*xs) + 0.5*sin(3*pi*xs) ;
 Ts     = T0 ;
 
+   
+% ------------------------
+% matrices assembly
 KdiffG = zeros( nnodes, nnodes ) ;
 MintEG = zeros( nnodes, nnodes ) ;
 
-% assembly
 for i = 1 : nelem
   nodeselem = [ i i+1 ] ;
   
   elemDofs = nodes2dofs ( nodeselem, 1 ) ;
   
   KdiffG( elemDofs , elemDofs ) = ...
-    KdiffG( elemDofs , elemDofs ) + Kdiffe  ; 
+  KdiffG( elemDofs , elemDofs ) + Kdiffe  ; 
   
-  MintEG( elemDofs , elemDofs ) = MintEG( elemDofs , elemDofs ) + MintEe  ; 
+  MintEG( elemDofs , elemDofs ) = ...
+  MintEG( elemDofs , elemDofs ) + MintEe  ; 
+
 end
+% ------------------------
 
 CDD = MintEG(diridofs, diridofs) ;
 CND = MintEG(neumdofs, diridofs) ;
 CNN = MintEG(neumdofs, neumdofs) ;
 
-qext = zeros(nnodes,1) ;
-
-%~ qext (1) = 1*Area ;
-%~ qext (end) = -1e1*Area ;
+qext = zeros( nnodes, 1 ) ;
 
 figure
-hold on
-plot( Ts(:,1) )  
+hold on, grid on
 
-for i=1:nt
-  
-  f = (    qext ( neumdofs    ) * dt ...
-        - KdiffG( neumdofs, : ) * Ts( :, i ) * dt ...
-        + MintEG( neumdofs, : ) * Ts( :, i ) 
-      ) ;
-      
-  eig(CNN)
-  Ts( neumdofs, i+1 ) = CNN \ f ;
-  Ts( diridofs, i+1 ) = Tdiri ;
-  
-  plot( Ts(:,i+1) );  
+MS = 10 ; 
+LW = 1.5 ; 
 
+% ------------------------
+% time loop
+for i=0:nt
+  
+  t = i*dt ;
+  
+  if i~=0
+    f = (    qext ( neumdofs    ) * dt ...
+          - KdiffG( neumdofs, : ) * Ts( :, i ) * dt ...
+          + MintEG( neumdofs, : ) * Ts( :, i ) 
+        ) ;
+        
+    Ts( neumdofs, i+1 ) = CNN \ f ;
+    Ts( diridofs, i+1 ) = Tdiri   ;
+  end  
+
+  TsAnly (:,i+1) = exp(-(  pi*alpha)^2 * t ) *       sin(     pi * xsAnly ) ...
+                 + exp(-(3*pi*alpha)^2 * t ) * 0.5 * sin( 3 * pi * xsAnly ) ;
+  
+  if mod(i, 10)==0
+    plot( xs    , Ts    (:, i+1), 'b-o', 'markersize', MS,'linewidth',LW );  
+    plot( xsAnly, TsAnly(:, i+1), 'r--'  , 'markersize', MS,'linewidth',LW );  
+  end
 end
+% ------------------------
