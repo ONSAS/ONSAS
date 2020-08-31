eeacd348f6869706e3830315df3c15ca1dfd7b3f
Jorge <jorgepz@fing.edu.uy>
2020-08-31 12:56:13 -0300

    convert 1D toy heat example to function

--- examples/aux_toyHeatTransferExample.m
+++ examples/aux_toyHeatTransferExample.m
@@ -2,13 +2,11 @@
 % Toy Heat Transfer Problem
 % ---------------------------
 
-clear all, close all
+function aux_toyHeatTransferExample(caseNum, nelem, plotBoolean )
 
 addpath('../sources/')
 
-nelem  = 5 ;
-Tfinal = .5 ;
-dt     = .001 ;
+dt     = 0.001 ;
 rho    = 1 ;
 cSpHe  = 1 ;
 kCond  = 1 ;
@@ -16,6 +14,39 @@ Ltot   = 1 ; % domain [0,1]
 Area   = 1 ;
 
 %~ nt     = 3 ; % defined if stopped before Tfinal
+
+
+switch caseNum
+
+case 1  % diri-diri conds
+
+  Tfinal      = 0.5   ;
+  %~ nelem       = 5     ;
+  diridofs    = [ 1 nelem+1 ] ;
+  Tdiri       = 0 ;
+  anlyBoolean = 1;
+  
+case 2 % diri-neum conds
+
+  %~ nelem  = 10     ;
+  Tfinal = 1   ;
+  diridofs = [ 1 ] ;
+  Tdiri  = 0 ;
+  anlyBoolean = 0;
+  qentr = -0 ;
+
+case 3 % diri-robin conds
+
+  nelem  = 10     ;
+  Tfinal = 1   ;
+  diridofs = [ 1 ] ;
+  Tdiri  = 0 ;
+  anlyBoolean = 0;
+  qentr = -0 ;
+   
+end
+
+
 
 if exist('nt')==0
   nt     = Tfinal / dt ;
@@ -30,8 +61,6 @@ nnodesAnly = 100 ;
 xs     = linspace(0, Ltot, nnodes     )' ;
 xsAnly = linspace(0, Ltot, nnodesAnly )' ;
 
-diridofs = [ 1 nnodes ] ;
-Tdiri  = 0 ;
 
 neumdofs = 1:nnodes ;
 neumdofs( diridofs ) = [] ;
@@ -72,9 +101,14 @@ CND = MintEG(neumdofs, diridofs) ;
 CNN = MintEG(neumdofs, neumdofs) ;
 
 qext = zeros( nnodes, 1 ) ;
+if caseNum == 2
+  qext(end) = qentr ;
+end
 
+if plotBoolean
 figure
 hold on, grid on
+end
 
 MS = 10 ; 
 LW = 1.5 ; 
@@ -82,7 +116,6 @@ LW = 1.5 ;
 % ------------------------
 % time loop
 for i=0:nt
-  
   t = i*dt ;
   
   if i~=0
@@ -95,20 +128,33 @@ for i=0:nt
     Ts( diridofs, i+1 ) = Tdiri   ;
   end  
 
-  TsAnly (:,i+1) = exp(-(  pi*alpha)^2 * t ) *       sin(     pi * xsAnly ) ...
-                 + exp(-(3*pi*alpha)^2 * t ) * 0.5 * sin( 3 * pi * xsAnly ) ;
-  
-  if mod(i, 10)==0
-    plot( xs    , Ts    (:, i+1), 'b-o', 'markersize', MS,'linewidth',LW );  
-    plot( xsAnly, TsAnly(:, i+1), 'r--'  , 'markersize', MS,'linewidth',LW );  
+  if anlyBoolean
+    TsAnly (:,i+1) = exp(-(  pi*alpha)^2 * t ) *       sin(     pi * xsAnly ) ...
+                   + exp(-(3*pi*alpha)^2 * t ) * 0.5 * sin( 3 * pi * xsAnly ) ;
   end
+  
+  % --- plots ---
+  if plotBoolean
+  
+    if mod(i, round(nt/10) )==0
+      plot( xs    , Ts    (:, i+1), 'b-o', 'markersize', MS,'linewidth',LW );  
+      
+      if anlyBoolean
+        plot( xsAnly, TsAnly(:, i+1), 'r--'  , 'markersize', MS,'linewidth',LW );  
+      end
+  
+    end
+  end
+    % ---------------
+  
 end
 % ------------------------
 
-figure
-plot( Ts(2,:) )
-
+if plotBoolean
+  figure
+  plot( Ts(2,:) )
+end
 
 KdiffGNN = KdiffG(neumdofs, neumdofs ) ;
 
-save -mat auxVars.mat KdiffGNN CNN
+save -mat auxVars.mat KdiffGNN CNN Ts
