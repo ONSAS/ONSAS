4ba998477c3fb4a3d1e4b1cdbec2a99f3136b5d8
Jorge <jorgepz@fing.edu.uy>
2020-09-03 06:57:56 -0300

    added paths to fix errors

--- examples/onsasExample_dxfTransmissionTower.m
+++ examples/onsasExample_dxfTransmissionTower.m
@@ -7,12 +7,10 @@
 clear all, close all
 
 %% General data
-dirOnsas = [ pwd '/..' ] ;
+dirOnsas = [ pwd '/..' ] ; addpath( [ dirOnsas ] );
 
 % Problem name in order to write LaTex report 
 problemName = 'dxfTransmissionTower' ; 
-
-addpath( [ dirOnsas ] );
 
 % scalar auxiliar parameters
 E = 210e9 ;  A = 2.5e-1 ; nu = 0 ;  rho = 0 ; 
@@ -21,14 +19,14 @@ E = 210e9 ;  A = 2.5e-1 ; nu = 0 ;  rho = 0 ;
 % MELCS parameters
 % Materials
 % structure of cell: [ density model YoungModulus Poisson coefficient ]
-materialsParams = {[ rho 1 E nu ]} ;
+materialsParams = {[ rho 3 E nu ]} ;
 % Elements
 % Structure of the cell: { elem_1 ; ... ; elem_n }
 elementsParams  = { 1; 2} ;
 % Loads
 % Structure of the cell: { [ loadLabel global/local Fx Mx Fy My Fz Mz ] }
-loadsParams     = { [ 1 1   0 0 0 0 -15e4 0] ;
-										[ 1 1   0 0 0 0 -5e4 0 ] } ;
+loadsParams     = { [ 1 1   0 0 0   0 -15e4 0] ;
+										[ 1 1   0 0 1e2 0 -5e4  0 ] } ;
 % Cross-Sections
 crossSecsParams = { [ 2 .2 .2 ] } ;
 % Springs
@@ -36,20 +34,21 @@ crossSecsParams = { [ 2 .2 .2 ] } ;
 springsParams   = { [ inf  0  inf  0  inf   0 ]} ;
 
 % Nodes and Conectivity matrix from .dxf file
+addpath( [ dirOnsas '/sources' ] );
 [ Nodes, Conec ] = meshFileReader( 'torreAltaTension.dxf' ) ;
 
+% analysis parameters
+stopTolDeltau    = 1.0e-8 ;    stopTolForces    = 1.0e-8 ;
+targetLoadFactr  = 1e2    ;    nLoadSteps       = 10     ;
+stopTolIts       = 30     ;
 
-% Analysis parameters
-nonLinearAnalysisBoolean 	= 0 ;
-dynamicAnalysisBoolean 		= 0 ;
+numericalMethodParams = [ 1 stopTolDeltau stopTolForces stopTolIts ...
+                            targetLoadFactr nLoadSteps ] ; 
 
+stabilityAnalysisBoolean = 1 ;
 
 % Output parameters
 plotParamsVector = [ 3 ] ;
-reportBoolean    = 1     ;
 
-
-acdir = pwd ;
-cd(dirOnsas);
+% run ONSAS
 ONSAS
-cd(acdir) ;
