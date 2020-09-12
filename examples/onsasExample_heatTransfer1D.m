6364f3540c794e6b091ac7cc23c16e3258a85166
Jorge <jorgepz@fing.edu.uy>
2020-09-12 14:14:31 -0300

    add heat transfer example and remove gifs

---
+++ examples/onsasExample_heatTransfer1D.m
@@ -0,0 +1,10 @@
+% ======================================================================
+% 
+
+clear all, close all
+dirOnsas    = [ pwd '/..' ] ; addpath( dirOnsas );
+problemName = 'heatTransfer1D' ;
+
+nelem = 10 ;
+
+aux_toyHeatTransferExample( 1, nelem, 1 ) ;
