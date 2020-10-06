97b23e76687cc16defcb55e714e251527a2e8fb7
Mauricio <mvanzulli@fing.edu.uy>
2020-10-06 14:04:37 -0300

    Update onsasExample_BeamTrussJoint_test.m
    Fixed input for different truss/beam elements

--- examples/onsasExample_BeamTrussJoint_test.m
+++ examples/onsasExample_BeamTrussJoint_test.m
@@ -26,6 +26,7 @@ crossSecsParams = {[3 dt] ; [3 db]} ;
 
 % Springs
 springsParams   = { [ inf  inf  inf  inf  inf   inf ] ; ...
+                    [ inf  inf  inf  inf   0     0 ] ; ...
                     [ inf    0  inf  0    inf   0 ] } ;
 
 %Nodes and Conec matrix:
@@ -36,7 +37,7 @@ NelemT  = 1;
 NnodesT = NelemT+1;
 
 %Bem elements
-NelemB  = 1;
+NelemB  = 2;
 NnodesB = NelemB+1;
 
 %Total elements
@@ -44,37 +45,37 @@ Nelem = NelemT+NelemB	;
 Nnodes = Nelem+1;
 
 
-Nodes = [ 	(0:(NelemT))'*Lb/NelemT  zeros(NnodesT,1)          zeros(NnodesT,1) 	   ;	 
-			Lb*ones(NelemB,1) 		 zeros(NelemB,1)         -(1:(NelemB))'*Lt/NelemB ] ;
+Nodes = [ 	(0:(NelemB))'*Lb/NelemB  zeros(NelemB+1,1)          zeros(NelemB+1,1) 	   ;	 
+			Lb*ones(NelemT,1) 		 zeros(NelemT,1)         -(1:(NelemT))'*Lt/NelemT ] ;
 													   
 				    %Material             %Element                   %Loads                 %CrossSection             %Springs 					 
-auxConecElem  = [ [(ones(NelemB,1)*2 )    (ones(NelemB,1)*3)      (ones(NelemB,1))         (ones(NelemB,1)*2)      (zeros(NelemB,1)) ...  %ElemNodes...
+auxConecElem  = [ [(ones(NelemB,1)*2 )    (ones(NelemB,1)*3)      (zeros(NelemB,1))         (ones(NelemB,1)*2)      (zeros(NelemB,1)) ...  %ElemNodes...
                  (1:(NelemB))'                 (2:NelemB+1)'                  zeros(NelemB,2)];
-                  [(ones(NelemT,1)*1 )    (ones(NelemT,1)*2)      (ones(NelemT,1))         (ones(NelemT,1)*1)      (zeros(NelemT,1)) ...  %ElemNodes...
-                 (NelemB+1:NelemB+NelemT)'	 	(NelemB+2:NelemB+NelemT+1)'   zeros(NelemB,2)] ] ;  
+                  [(ones(NelemT,1)*1 )    (ones(NelemT,1)*2)      (zeros(NelemT,1))         (ones(NelemT,1)*1)      (zeros(NelemT,1)) ...  %ElemNodes...
+                 (NelemB+1:NelemB+NelemT)'	 	(NelemB+2:NelemB+NelemT+1)'   zeros(NelemT,2)] ] ;  
                         %Material             %Element                   %Loads                 %CrossSection             %Springs 			%Nodes  
 auxConecNodes = [           0                   1                           0                           0                   1                   1   
-                            0                   1                           1                           0                   0                NnodesB 
-                            0                   1                           0                           0                   2                 Nnodes ];                                                                                                                                 
+                            0                   1                           1                           0                   2                NnodesB 
+                            0                   1                           0                           0                   3                 Nnodes ];                                                                                                                                 
 
 
 %Build Conec Cell
-Conec = cell (Nelem+Nnodes,1);
-for i = 1:Nnodes
+Conec = cell (Nelem+size(auxConecNodes,1),1);
+for i = 1:size(auxConecNodes,1)
     Conec{i,1}=auxConecNodes(i,:) ;
 end
 for i =  1:Nelem
-    Conec{Nnodes+i,1} =auxConecElem(i,:);
+    Conec{size(auxConecNodes,1)+i,1} =auxConecElem(i,:);
 end
 
-controlDofs      = [ 2 5 -1 ] ; % [ node nodaldof scalefactor ]
+controlDofs      = [ NnodesB 5 -1 ] ; % [ node nodaldof scalefactor ]
 plotParamsVector = [ 3 ] ;
 reportBoolean    = 1     ;
 
 
 % analysis parameters
 stopTolDeltau    = 1.0e-8 ;    stopTolForces    = 1.0e-8 ;
-targetLoadFactr  = 2.0e10  ;    nLoadSteps       = 20      ;
+targetLoadFactr  = 2.0e11  ;    nLoadSteps       = 20      ;
 stopTolIts       = 30     ;
 
 
