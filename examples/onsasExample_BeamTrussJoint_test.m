c15d77eb7092694eb7fb6c9d2b4132324b609c93
Mauricio <mvanzulli@fing.edu.uy>
2020-10-19 13:08:45 -0300

    Validated and fixed elementBeamForces.m
    Fixed bug and validated beam and truss mixed elements

--- examples/onsasExample_BeamTrussJoint_test.m
+++ examples/onsasExample_BeamTrussJoint_test.m
@@ -5,9 +5,9 @@ close all, clear all %#ok
 problemName = 'TurssBeamJoint_NR' ; 
 % ----------------------------------------------------------------------
 %Geometric properties truss:
-Et = 210e5 ; dt = 0.03; At = pi*dt^2/4 ;  Lt = 1 ; nut = 0.3 ;  rhot = 8000 ; 
+Et = 1e9 ; dt = .05; At = pi*dt^2/4 ;  Lt = 1 ; nut = 0.3 ;  rhot = 1 ; 
 %Geometric properties beam
-Eb = 210e7 ; db = 0.1; Ab = pi*db^2/4 ;  Lb = 10 ; nub = 0.3 ;  rhob = 1000 ; 
+Eb = Et/3 ;db = 5*dt ; Ab = pi*db^2/4 ;  Lb = .5 ; nub = 0.3 ;  rhob = 1 ; 
 Ib = pi*db^4/64 ;
 % ----------------------------------------------------------------------
 % MELCS parameters
@@ -19,15 +19,15 @@ materialsParams = {[ rhot 1 Et nut ];[rhob 1 Eb nub ]} ;
 elementsParams  = {1;2;3} ;
 
 % Loads
-loadsParams     = { [ 1 1   0 0 0 0 -1 0] } ;
+loadsParams     = { [ 1 1   0 0 0 0 +1 0] } ;
 
 % Cross-Sections
 crossSecsParams = {[3 dt] ; [3 db]} ;	
 
 % Springs
-springsParams   = { [ inf  inf  inf  inf  inf   inf ] ; ...
-                    [ inf  inf  inf  inf   0     0 ] ; ...
-                    [ inf    0  inf  0    inf   0 ] } ;
+springsParams   = { [ inf  inf    inf  inf  inf   inf  ] ; ...
+                    [  0   inf    inf   0    0     inf ] ; ...
+                    [ inf  inf    inf   inf    inf   0 ] } ;
 
 %Nodes and Conec matrix:
 %-----------------------
@@ -37,7 +37,7 @@ NelemT  = 1;
 NnodesT = NelemT+1;
 
 %Bem elements
-NelemB  = 2;
+NelemB  = 1;
 NnodesB = NelemB+1;
 
 %Total elements
@@ -68,20 +68,25 @@ for i =  1:Nelem
     Conec{size(auxConecNodes,1)+i,1} =auxConecElem(i,:);
 end
 
-controlDofs      = [ NnodesB 5 -1 ] ; % [ node nodaldof scalefactor ]
+controlDofs      = [ NnodesB 5 1 ] ; % [ node nodaldof scalefactor ]
 plotParamsVector = [ 3 ] ;
 reportBoolean    = 1     ;
 storeBoolean     = 1     ;
 
 % analysis parameters
-stopTolDeltau    = 1.0e-8 ;    stopTolForces    = 1.0e-8 ;
-targetLoadFactr  = 2.0e11  ;    nLoadSteps       = 20      ;
-stopTolIts       = 30     ;
-
+stopTolDeltau    = 1.0e-12 ;    stopTolForces    = 1.0e-12;
+targetLoadFactr  = 1e5 ;    nLoadSteps       = 10      ;
+stopTolIts       = 10     ;
+global flagOutputMatrices;
+flagOutputMatrices= 1 ;
 
 numericalMethodParams = [ 1 stopTolDeltau stopTolForces stopTolIts ...
                             targetLoadFactr nLoadSteps ] ; 
 
+%Analytic Solution
+analyticFunc   = @(w)(Et*At/Lt+3*Eb*Ib/Lb^3)*w;
+beamTruss_Ratio= Et*At/Lt/(3*Eb*Ib/Lb^3)
+                        
 run( [ pwd '/../ONSAS.m' ] ) ;
 
 
@@ -102,25 +107,25 @@ for i =  1:Nelem
 end
 
 % arc length params
-targetLoadFactrNRAL   = 2e11  ;
-incremArcLen          = 1e-2  ;
+targetLoadFactrNRAL   = targetLoadFactr  ;
+incremArcLen          = controlDispsNR(end)- controlDispsNR(end-1) ;%Same DeltaU step
 numericalMethodParams = [ 2 stopTolDeltau stopTolForces stopTolIts ...
                             targetLoadFactrNRAL nLoadSteps incremArcLen ] ; 
                         
-run( [ pwd '/../ONSAS.m' ] ) ;
-controlDispsNRAL = controlDisps ;
-loadFactorsNRAL  = loadFactors ;
+ run( [ pwd '/../ONSAS.m' ] ) ;
+controlDispsAL = controlDisps ;
+loadFactorsAL  = loadFactors ;
 
-analyticFunc =@(w) (Et*At/Lt+3*Eb*Ib/Lb^3)*w;
+
 
 % ----------------------------------------------------------------------
 % --- plots ---
 lw = 2.0 ; ms = 11 ; plotfontsize = 22 ;
 
 figure
-plot( controlDispsNRAL, analyticFunc(controlDispsNRAL),'b-x' , 'linewidth', lw,'markersize',ms)
+plot( controlDispsNR, analyticFunc(controlDispsNR),'b-x' , 'linewidth', lw,'markersize',ms)
 hold on, grid on
-plot( controlDispsNRAL, loadFactorsNRAL,'r-s' , 'linewidth', lw,'markersize',ms )
+plot( controlDispsAL, loadFactorsAL,'r-s' , 'linewidth', lw,'markersize',ms )
 plot( controlDispsNR, loadFactorsNR,'k-o' , 'linewidth', lw,'markersize',ms )
 
 labx = xlabel('Displacement');   laby = ylabel('\lambda') ;
