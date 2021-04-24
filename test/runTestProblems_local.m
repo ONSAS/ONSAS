410369847d03fe2a6db3373ebd4ca49c8b3bb561
jorgepz <jorgepz@fing.edu.uy>
2021-04-24 13:20:46 -0300

    fix local tests

--- test/runTestProblems_local.m
+++ test/runTestProblems_local.m
@@ -1,56 +1,64 @@
 
 close all, clear all %#ok
-keyword = '_test' ;
 
 if isunix, dirSep = '/'; else dirSep = '\'; end
 addpath( [ pwd  dirSep '..' dirSep  'src' dirSep ] ); octaveBoolean = isThisOctave ;
 
-if octaveBoolean
-  fileslist = readdir('../examples/');
-else
-  auxMatlab = dir('../examples')                  ;
-  fileslist = cell(length( auxMatlab ),1) ;
-  for k=1:length( auxMatlab )
-    fileslist{k} = auxMatlab(k).name ;
-  end
-end
 
-keyfiles = cell(length(fileslist),1); totalRuns = 0  ;
+%~ if octaveBoolean
+  %~ fileslist = readdir('../examples/');
+%~ else
+  %~ auxMatlab = dir('../examples')                  ;
+  %~ fileslist = cell(length( auxMatlab ),1) ;
+  %~ for k=1:length( auxMatlab )
+    %~ fileslist{k} = auxMatlab(k).name ;
+  %~ end
+%~ end
 
-for i=1:length(fileslist)
-  if length(fileslist{i})>5,
-    lastFiveChars = fileslist{i}; lastFiveChars = lastFiveChars((end-4:end)) ;
-    if strcmp( lastFiveChars, keyword )
-      totalRuns             = totalRuns +1 ;
-      keyfiles{ totalRuns } = fileslist{i} ;
-    end
-  end
-end
+
+%~ for i=1:length(fileslist)
+  %~ if length(fileslist{i})>5,
+    %~ lastFiveChars = fileslist{i}; lastFiveChars = lastFiveChars((end-4:end)) ;
+    %~ if strcmp( lastFiveChars, keyword )
+      %~ totalRuns             = totalRuns +1 ;
+      %~ keyfiles{ totalRuns } = fileslist{i} ;
+    %~ end
+  %~ end
+%~ end
+
+keyfiles = {'staticVonMisesTruss/onsasExample_staticVonMisesTruss.m'; 
+            'uniformCurvatureCantilever/onsasExample_uniformCurvatureCantilever.m' } ;
+
 
 current  = 1 ;   verifBoolean = 1 ;  testDir = pwd ;
 
-while current <= totalRuns && verifBoolean == 1
+while current <= length(keyfiles) && verifBoolean == 1
 
   % run current example
   fprintf([' === running script: ' keyfiles{current} '\n' ]);
   
   %~ run( [ pwd dirSep '..' dirSep 'examples' dirSep keyfiles{current} dirSep 'onsasExample_' keyfiles{current} '.m' ] ) ;
 
-  cd( [ pwd dirSep '..' dirSep 'examples' dirSep keyfiles{current} dirSep ] )
+  %~ cd( [ pwd dirSep '..' dirSep 'examples' dirSep keyfiles{current} dirSep ] )
 
   % save key files data to avoid clear all commands
-  save( '-mat', 'exData.mat', 'current', 'totalRuns', 'keyfiles', 'dirSep', 'testDir' );
+  save( '-mat', 'exData.mat', 'current', 'keyfiles', 'dirSep', 'testDir' );
 
-  run( [ 'onsasExample_' keyfiles{current} '.m' ] ) ;
+  run( [ pwd dirSep '..' dirSep 'examples' dirSep keyfiles{current} ] ) ;
   
   if verifBoolean
-    fprintf([' === test ' problemName ' problem:  PASSED === \n\n']);
+    status = 'PASSED';    
   else
-    fprintf([' === test ' problemName ' problem FAILED   === \n\n']);
+    status = 'FAILED';
   end
   
   % reload key files data and increment current
-  load('exData.mat') ;  current = current + 1 ; delete('exData.mat');
+  load('exData.mat') ;
+  
+  fprintf([' === test problem %2i:  %s === \n\n'], current, status );
+  
+  current = current + 1 ;
+  delete('exData.mat');
   cd ( testDir )
 end
 
