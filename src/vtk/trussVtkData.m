af2355cb1136245160c58338e165fb644c8fa915
jorgepz <jorgepz@fing.edu.uy>
2021-11-23 19:19:51 -0300

    fix vtk truss

--- src/vtk/trussVtkData.m
+++ src/vtk/trussVtkData.m
@@ -8,25 +8,39 @@ function [ vtkNodes, vtkConec, vtkNodalDisps, vtkNormalForces ] ...
 
   nelem = size(Conec,1) ;
 
+  counterNodes     = 0 ;
+
   for i=1:nelem
     nodesElem    = Conec(i,1:2) ;
     dofsElem     = nodes2dofs( nodesElem, 6 ) ;
-    coordSubElem = reshape( Nodes( nodesElem(:), : )', 6,1) ;
+
+    coordsElemNodes = reshape( Nodes( nodesElem(:), : )', 6, 1 ) ;
+
+    % length of current element
+    elemLength = norm( coordsElemNodes(4:6) - coordsElemNodes(1:3) ) ;
 
     % q section tengo
     [ iniNodes, midNodes, endNodes, sectPar ] = crossSectionVtkSolidConnec( elemTypeGeom ) ;
 
-    nodalDisp = U( dofsElem(1:2:end) ) ;
-    nodalRot  = zeros(6,1)             ;
+    dispsElem          = U( dofsElem ) ;
+    thetaLocIniSubElem = zeros(3,1)    ;
+    thetaLocEndSubElem = zeros(3,1)    ;
+    dispLocIniSubElem  = zeros(3,1)    ;
+    dispLocEndSubElem  = zeros(3,1)    ;
 
-    [ NodesCell, ConecCell ] = vtkBeam2SolidConverter ( coordSubElem, nodalDisp, nodalRot, sectPar ) ;
+    [ R0, Rr, locDisp ] = elementBeamRotData( coordsElemNodes, dispsElem ) ;
 
-    % add number of previous nodes to the new nodes
-    lastNode = size( vtkNodes, 1 ) ;
-    ConecCell(:,2:end) = ConecCell(:,2:end) + lastNode ;
+    coordLocSubElem = [ 0; elemLength ] ;
 
-    vtkNodes = [ vtkNodes ; NodesCell ] ;
-    vtkConec = [ vtkConec ; ConecCell ] ;
-    vtkNodalDisps = [ vtkNodalDisps ; [ ones(4,1)*nodalDisp(1:3)'; ones(4,1)*nodalDisp(4:6)' ] ] ;
+    [ Nodesvtk, Conecvtk, Dispsvtk ] = vtkBeam2SolidConverter( coordsElemNodes, ...
+       dispsElem, coordLocSubElem, dispLocIniSubElem, dispLocEndSubElem, thetaLocIniSubElem, thetaLocEndSubElem, sectPar, Rr, R0 ) ;
+
+
+       Conecvtk( :, 2:end ) = Conecvtk(:,2:end)+counterNodes ;
+       vtkNodes             = [ vtkNodes ;     Nodesvtk ] ;
+       vtkConec             = [ vtkConec ;     Conecvtk ] ;
+       vtkNodalDisps        = [ vtkNodalDisps; Dispsvtk ] ;
+
+       counterNodes = counterNodes + 8 ;
 
   end % for plot element subdivision
