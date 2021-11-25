f2cd317e212f76a6b10e25f9c448ae3b19efb7e7
mvanzulli <mvanzulli@fing.edu.uy>
2021-11-25 20:06:16 -0300

    added restrctions to aerodynamic coefficients

--- examples/linearStaticCantileverAero/dragCoefFunction.m
+++ examples/linearStaticCantileverAero/dragCoefFunction.m
@@ -1,8 +1,11 @@
 function C_d = dragCoefFunction (betaRel)
     % Aeroelastic stability of a 3DOF system based on quasi-steady theory with reference to inertial coupling
     % Emulation of drag extrated from the reference above
-    if max(betaRel)< deg2rad(210)
-        omega_drag = 0.8 * 2 * pi / deg2rad(180);
-        C_d = +1.2 - 0.6* cos( omega_drag * betaRel);
+    if 0<betaRel< deg2rad(210)
+      omega_drag = 0.8 * 2 * pi / deg2rad(180);
+      C_d = +1.2 - 0.6* cos( omega_drag * betaRel);
+    else 
+      betaRel
+      error ("The angle must be beteween 0º and 210 to use this aerodynamic coefficents")
     end
 end
