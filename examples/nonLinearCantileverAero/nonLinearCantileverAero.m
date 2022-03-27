e242ad8696f5042bf7019b7593fabc495edd0e44
mvanzulli <mvanzulli@fing.edu.uy>
2022-03-27 14:16:19 -0300

    implemented nonLinearAero docs

--- examples/nonLinearCantileverAero/nonLinearCantileverAero.m
+++ examples/nonLinearCantileverAero/nonLinearCantileverAero.m
@@ -27,7 +27,7 @@ close all, clear all ; addpath( genpath( [ pwd '/../../src'] ) );
 E = 1e9 ;  nu = 0.3 ; rho = 1800 ; G = E / (2 * (1+nu)) ;
 l = 10 ; d = l/100 ; J = pi * d ^ 4 / 32 ; Iyy = J / 2 ; Izz = Iyy ;  
 %md next the number of frame elements for all cases is set:
-numElements = 3 ;
+numElements = 10 ;
 %md### MEBI parameters
 %md
 %md### materials
@@ -47,8 +47,8 @@ elements(2).elemCrossSecParams{2,1} = d        ;
 %md Now the element aerodynmamic properties are defined. First the drag coefficient function located at the folder's example is declared into _userDragCoef_ field as:
 elements(2).userDragCoef   = 'dragCoefCircular'   ;
 %md Next the _elemTypeAero_ field contains the information of the chord vector. This vector is defined first considering the orientation of the cross section set up for drag experiments. According to the revolution symmetry of the problem the chord vector orientation has no impact into drag force vector, since $c_d$ is constant any angle of incidence. However the characteristic dimension of the circular cross section is declared into the norm of the chord vector ( first three entries of _elemTypeAero_ field into `elements` struct ) as: 
-numGaussPoints  = 4 ;
-elements(2).elemTypeAero   = [0 d 0 numGaussPoints 4 ] ;
+numGaussPoints           = 4 ;
+elements(2).elemTypeAero = [0 d 0 numGaussPoints ] ;
 %md also 4 number of integration Gauss points are employed to compute each element aerodynamic force vector.
 %md
 %md### boundaryConds
@@ -123,7 +123,7 @@ thetaZdefNum = matUsSD(6:6:end,end)   ;
 %md### Plot verification
 %md
 %md The plot parameters are:
-lw = 3 ; ms = 9 ;
+lw = 2 ; ms = 12 ;
 labelTitle = [' Validating solution with ' num2str(numElements) ' elements' ] ;
 axislw = 2 ; axisFontSize = 20 ; legendFontSize = 15 ; curveFontSize = 15 ;    
 folderPathFigs = './output/figs/' ;
@@ -185,78 +185,78 @@ elements(2).elemTypeParams = 1 ;
 %md The name of this case problem is:
 otherParams.problemName = 'nonLinearCantileverLDStatic' ;
 %md
-% %md ONSAS software is executed for the parameters above defined and the displacement solution of each load(time) step is saved into matUsLDStatic matrix:
-% [matUsLDStatic, ~] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
-% %md--------------------
-% %md### Numerical solution dynamic case
-% %md---------------------
-% %md Next a dynamic example considering large displacements motion is addressed to test the convergence of the dynamic solution disregarding any artificial damping. 
-% %md
-% %md### analysisSettings  
-% %md For such propose the wind velocity function name is now: 
-% analysisSettings.userWindVel = 'windVelNonLinearDynamic' ;
-% %md Inside that function a ramp velocity profile $v_a(t) = 5*t*(t<10) + 5*t*(t>=10)$ is declared. This is an abrupt wind velocity load from 0 to $7$ m/s in $10$ s .
-% %md
-% %md Regarding the integration time method scheme, a classic Newmark trapezoidal is set as:  
-% analysisSettings.deltaT     =  1        ;
-% analysisSettings.finalTime  =  200      ;
-% analysisSettings.methodName = 'newmark' ;
-% analysisSettings.alphaNM    =  0.25     ;
-% analysisSettings.deltaNM    =  0.5      ;
-% %md
-% %md### otherParams
-% %md The name of this case problem is:
-% otherParams.problemName = 'nonLinearCantileverLDDynamic2D' ;
-% %md
-% %md ONSAS software is executed for the parameters above defined and the displacement solution of each load(time) step is saved into matUsLDDynamic matrix:
-% [matUsLDDynamic, ~] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
-% %md 
-% %md### Verification
-% %md---------------------
-% %md The numerical dynamic convergence to the static solution is then verified . The degree of freedom selected for such task is $u_y(t)$ of node A. 
-% %md
-% %md### Static solution.
-% %md
-% %md Extract static numerical time history displacements $u_y$ of node A. First the selected degree of freedom is:
-% nodeA = numElements + 1                ;
-% relativeDofUyA = 3                     ;
-% dofUyA = (nodeA -1)*6 + relativeDofUyA ; 
-% %md then node A $u_y$ time history accessed by:
-% UyAStaticSol = matUsLDStatic(dofUyA,:) ;
-% %md
-% %md### Dynamic solution.
-% %md
-% %md Extract dynamic numerical solution as follows:
-% UyADynamicSol = matUsLDDynamic(dofUyA,:) ;
-% %md next, the time vector is given by:
-% timVecLD = linspace(0, analysisSettings.finalTime, size(matUsLDDynamic,2) ) ;
-% %md
-% %md### Verification Plot
-% %md
-% %md Create folder to save figures
-% folderFigs = strcat('./output/', 'figs/') ;
-% mkdir(folderFigs) ;
-% %md The linear $u_y$ displacements verification of node  A is finally plotted executing:  
-% fig3 = figure(3) ;
-% hold on,  grid on
-% % legend first point plot
-% plot(timVecLD(1), UyADynamicSol(1),...
-%      'color', 'b', 'linewidth', lw, 'linestyle', '-','markersize', ms, 'marker', 'o')
-% % static solution plot
-% plot(timVecLD   , UyAStaticSol(end)*ones(length(timVecLD)), 'k:' , 'linewidth', lw, 'markersize', ms     );
-% % markers plot
-% plot(timVecLD(1:8:end), UyADynamicSol(1:8:end),...
-%      'color', 'b', 'linewidth', lw, 'linestyle', 'none','markersize', ms, 'marker', 'o')
-% % continium line plot
-% plot(timVecLD, UyADynamicSol,...
-%      'color', 'b', 'linewidth', lw, 'linestyle', '-', 'marker', 'none')
-% legend('dynamic LD', 'static LD' )
-% labx=xlabel('x[m]');    laby=ylabel('y[m]');
-% set(legend, 'linewidth', axislw, 'fontsize', legendFontSize, 'location','northEast' ) ;
-% set(gca, 'linewidth', axislw, 'fontsize', curveFontSize ) ;
-% set(labx, 'FontSize', axisFontSize); set(laby, 'FontSize', axisFontSize) ;
-% namefig3 = strcat(folderPathFigs, 'uyA.png') ;
-% print(fig3, namefig3,'-dpng') ;
+%md ONSAS software is executed for the parameters above defined and the displacement solution of each load(time) step is saved into matUsLDStatic matrix:
+[matUsLDStatic, ~] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
+%md--------------------
+%md### Numerical solution dynamic case
+%md---------------------
+%md Next a dynamic example considering large displacements motion is addressed to test the convergence of the dynamic solution disregarding any artificial damping. 
+%md
+%md### analysisSettings  
+%md For such propose the wind velocity function name is now: 
+analysisSettings.userWindVel = 'windVelNonLinearDynamic2D' ;
+%md Inside that function a ramp velocity profile $v_a(t) = 5*t*(t<6.6) + 5*t*(t>=6.6)$ is declared. This is an abrupt wind velocity load from 0 to $7$ m/s in $10$ s .
+%md
+%md Regarding the integration time method scheme, a classic Newmark trapezoidal is set as:  
+analysisSettings.deltaT     =  1        ;
+analysisSettings.finalTime  =  200      ;
+analysisSettings.methodName = 'newmark' ;
+analysisSettings.alphaNM    =  0.25     ;
+analysisSettings.deltaNM    =  0.5      ;
+%md
+%md### otherParams
+%md The name of this case problem is:
+otherParams.problemName = 'nonLinearCantileverLDDynamic2D' ;
+%md
+%md ONSAS software is executed for the parameters above defined and the displacement solution for each time step is saved into matUsLDDynamic matrix:
+[matUsLDDynamic, ~] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
+%md 
+%md### Verification
+%md---------------------
+%md The numerical dynamic convergence to the static solution is then verified . The degree of freedom selected for such task is $u_y(t)$ of node A. 
+%md
+%md### Static solution.
+%md
+%md Extract static numerical time history displacements $u_y$ of node A. First the selected degree of freedom is:
+nodeA = numElements + 1                ;
+relativeDofUyA = 3                     ;
+dofUyA = (nodeA -1)*6 + relativeDofUyA ; 
+%md then node A $u_y$ time history accessed by:
+UyAStaticSol = matUsLDStatic(dofUyA,:) ;
+%md
+%md### Dynamic solution.
+%md
+%md Extract dynamic numerical solution as follows:
+UyADynamicSol = matUsLDDynamic(dofUyA,:) ;
+%md next, the time vector is given by:
+timVecLD = linspace(0, analysisSettings.finalTime, size(matUsLDDynamic,2) ) ;
+%md
+%md### Verification Plot
+%md
+%md Create folder to save figures
+folderFigs = strcat('./output/', 'figs/') ;
+mkdir(folderFigs) ;
+%md The linear $u_y$ displacements verification of node  A is finally plotted executing:  
+fig3 = figure(3) ;
+hold on,  grid on
+% legend first point plot
+plot(timVecLD(1), UyADynamicSol(1),...
+     'color', 'b', 'linewidth', lw, 'linestyle', '-','markersize', ms, 'marker', 'o')
+% static solution plot
+plot(timVecLD   , UyAStaticSol(end)*ones(length(timVecLD)), 'k:' , 'linewidth', lw, 'markersize', ms     );
+% markers plot
+plot(timVecLD(1:8:end), UyADynamicSol(1:8:end),...
+     'color', 'b', 'linewidth', lw, 'linestyle', 'none','markersize', ms, 'marker', 'o')
+% continium line plot
+plot(timVecLD, UyADynamicSol,...
+     'color', 'b', 'linewidth', lw, 'linestyle', '-', 'marker', 'none')
+legend('dynamic LD', 'static LD' )
+labx=xlabel('x[m]');    laby=ylabel('y[m]');
+set(legend, 'linewidth', axislw, 'fontsize', legendFontSize, 'location','northEast' ) ;
+set(gca, 'linewidth', axislw, 'fontsize', curveFontSize ) ;
+set(labx, 'FontSize', axisFontSize); set(laby, 'FontSize', axisFontSize) ;
+namefig3 = strcat(folderPathFigs, 'uyA.png') ;
+print(fig3, namefig3,'-dpng') ;
 %md
 %md```@raw html
 %md<img src="./assets/nonLinearCantileverAero/uyA.png" alt="plot check angular displacements" width="500"/>
@@ -264,8 +264,7 @@ otherParams.problemName = 'nonLinearCantileverLDStatic' ;
 %md
 %md## Large displacements 3D case
 %md--------------------
-%md A large dipslacements dynamic 3D case is presented as follows. This example is inspierd on [Vortex-Induced Vibration of Soft Corals](https://www.youtube.com/watch?v=Lf9Ffj5rGh8&ab_channel=FrederickGosselin).
-%md---------------------
+%md A large displacements dynamic 3D case is presented as follows. This example is inspired on [Vortex-Induced Vibration of Soft Corals](https://www.youtube.com/watch?v=Lf9Ffj5rGh8&ab_channel=FrederickGosselin).
 %md### MEBI parameters
 %md---------------------
 %md
@@ -274,17 +273,17 @@ otherParams.problemName = 'nonLinearCantileverLDStatic' ;
 materials.hyperElasParams = [ 1e8 nu ]       ;
 %md
 %md### analysisSettings  
-%md Regarding the integration time method scheme, a classic $\alpha-HHT$ method is employed. This method is more numercially stable than Newmark, the keen reader is refereed to [this reference](https://onlinelibrary.wiley.com/doi/abs/10.1002/eqe.4290050306):
+%md Regarding the integration time method scheme, a classic $\alpha-HHT$ method is employed. This method is more stable numercially than Newmark, the keen reader is refereed to [this reference](https://onlinelibrary.wiley.com/doi/abs/10.1002/eqe.4290050306):
 analysisSettings.methodName = 'alphaHHT';
 analysisSettings.alphaHHT   =   -0.05   ;
 %md the simulation time is defined such that:  
-analysisSettings.deltaT     =  .1      ;
-analysisSettings.finalTime  =  120       ;
-%md The emulation of the vortex shedding vibration is generated by a synthetic wind veloctiy composed by two sinusoidal velocities. First a low frequency sinous is implemente along the mean flow direction $y$ and then a high frequency component  along $z$. The high frequency component is selected to produce resonance between the flow and the beam,  theafter the first mode frequency is calculated as follows:
+analysisSettings.deltaT     =  .2  ;
+analysisSettings.finalTime  =  120 ;
+%md The emulation of the vortex shedding vibration is generated by a synthetic wind velocity composed by two sinusoidal velocities. A low frequency $Vy_a$ along the mean flow direction $y$ and then a high frequency component  $Vz_a$ along $z$. The high frequency component is selected to produce resonance effects between the flow and the beam, thus the high frequency velocity is selcted equal to the first mode bending:
 freqBendingFirstMode = (1.875)^2 * sqrt( materials.hyperElasParams(1) * Iyy / (materials.density * (pi * d^4 / 4) * l^4) ) ;
 analysisSettings.userWindVel = 'windVelNonLinearDynamic3D' ;
-%md The velocity function is assambled and plotted to ilustrate the reader
-timeVecLD3d                 = linspace(0,analysisSettings.finalTime, analysisSettings.finalTime / analysisSettings.deltaT + 1) ;
+%md The velocity function componentes are assembled:
+timeVecLD3d = linspace(0,analysisSettings.finalTime, ceil(analysisSettings.finalTime / analysisSettings.deltaT + 1) ) ;
 windVelY = [] ; windVelZ = [] ;
 for timeIndex = timeVecLD3d
     windVelVecTimeIndex = feval(analysisSettings.userWindVel, 0, timeIndex) ;
@@ -296,35 +295,83 @@ end
 %md The name of this case problem is:
 otherParams.problemName = 'nonLinearCantileverLD3D' ;
 %md
-%md ONSAS software is executed for the parameters above defined and the displacement solution of each load(time) step is saved into matUsLD3D matrix:
+%md ONSAS software is executed for the parameters above defined and the displacement solution for each time step is saved into matUsLD3D matrix:
 [matUsLD3D, ~] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
-md 
 %md
 %md### Plots
 %md
-%md The wind velocty profile implemente plotted executing:  
-fig3 = figure(3) ;
+%md The wind velocity profile implemented is plotted executing:  
+fig4 = figure(4) ;
 hold on,  grid on
 % legend first point plot
-plot(timeVecLD3d, windVelY(1),...
+plot(timeVecLD3d(1), windVelY(1),...
      'color', 'b', 'linewidth', lw, 'linestyle', '-','markersize', ms, 'marker', 'o')
-plot(timeVecLD3d, windVelZ(1),...
-     'color', 'r', 'linewidth', lw, 'linestyle', '-','markersize', ms, 'marker', 'o')
+plot(timeVecLD3d(1), windVelZ(1),...
+     'color', 'r', 'linewidth', lw, 'linestyle', '-','markersize', ms, 'marker', '^')
 % markers plot
-plot(timeVecLD3d(1:8:end), windVelY(1:8:end),...
+plot(timeVecLD3d(1:10:end), windVelY(1:10:end),...
      'color', 'b', 'linewidth', lw, 'linestyle', 'none','markersize', ms, 'marker', 'o')
-plot(timeVecLD3d(1:8:end), windVelZ(1:8:end),...
-     'color', 'r', 'linewidth', lw, 'linestyle', 'none','markersize', ms, 'marker', 'o')
+plot(timeVecLD3d(1:17:end), windVelZ(1:17:end),...
+     'color', 'r', 'linewidth', lw, 'linestyle', 'none','markersize', ms, 'marker', '^')
 % continium line plot
 plot(timeVecLD3d, windVelY,...
      'color', 'b', 'linewidth', lw, 'linestyle', '-', 'marker', 'none')
 plot(timeVecLD3d, windVelZ,...
      'color', 'r', 'linewidth', lw, 'linestyle', '-', 'marker', 'none')
-legend('Vy_a', 'Vz_a' )
-labx=xlabel('x[m]');    laby=ylabel('y[m]');
+legend('Va_y', 'Va_z' )
+labx=xlabel('t[s]');    laby=ylabel('V_a[m/s]');
 set(legend, 'linewidth', axislw, 'fontsize', legendFontSize, 'location','northEast' ) ;
 set(gca, 'linewidth', axislw, 'fontsize', curveFontSize ) ;
 set(labx, 'FontSize', axisFontSize); set(laby, 'FontSize', axisFontSize) ;
-namefig3 = strcat(folderPathFigs, 'uyA.png') ;
-print(fig3, namefig3,'-dpng') ;
-md
+namefig4 = strcat(folderPathFigs, 'windVel3D.png') ;
+axis([0,50])
+print(fig4, namefig4,'-dpng') ;
+%md
+%md```@raw html
+%md<img src="./assets/nonLinearCantileverAero/windVel3D.png" alt="plot check angular displacements" width="500"/>
+%md```
+%md
+%md Then $u_y$ of node A  is computed using:  
+UyADynamicSol3D = matUsLD3D(dofUyA,:) ;
+%md analogosuly $u_z$ of A node is:  
+UzADynamicSol3D = matUsLD3D(dofUyA + 2,:) ;
+%md
+%md Open figure and plot
+fig5 = figure(5) ;
+hold on,  grid on
+% legend first point plot uy 
+plot(timeVecLD3d(1), UyADynamicSol3D(1),...
+     'color', 'b', 'linewidth', lw, 'linestyle', '-','markersize', ms, 'marker', 'o')
+% legend first point plot uz
+plot(timeVecLD3d(1), UzADynamicSol3D(1),...
+     'color', 'r', 'linewidth', lw, 'linestyle', '-','markersize', ms, 'marker', '^')
+% markers plot uy
+plot(timeVecLD3d(1:13:end), UyADynamicSol3D(1:13:end),...
+     'color', 'b', 'linewidth', lw, 'linestyle', 'none','markersize', ms, 'marker', 'o')
+% continium line plot uy
+plot(timeVecLD3d, UyADynamicSol3D,...
+     'color', 'b', 'linewidth', lw, 'linestyle', '-', 'marker', 'none')
+% markers plot uz
+plot(timeVecLD3d(1:23:end), UzADynamicSol3D(1:23:end),...
+     'color', 'r', 'linewidth', lw, 'linestyle', 'none','markersize', ms, 'marker', '^')
+% continium line plot uz
+plot(timeVecLD3d, UzADynamicSol3D,...
+     'color', 'r', 'linewidth', lw, 'linestyle', '-', 'marker', 'none')
+legend('U_y node A', 'U_z node A' )
+labx=xlabel('x[m]');    laby=ylabel('Dispalcements[m]');
+set(legend, 'linewidth', axislw, 'fontsize', legendFontSize, 'location','northEast' ) ;
+set(gca, 'linewidth', axislw, 'fontsize', curveFontSize ) ;
+set(labx, 'FontSize', axisFontSize); set(laby, 'FontSize', axisFontSize) ;
+namefig5 = strcat(folderPathFigs, 'uA3D.png') ;
+print(fig5, namefig5,'-dpng') ;
+%md
+%md```@raw html
+%md<img src="./assets/nonLinearCantileverAero/uA3D.png" alt="plot check angular displacements" width="500"/>
+%md```
+%md
+%md Finally a GIF to illustrate the motion amplitude is subsequently presented:
+%md
+%md```@raw html
+%md<img src="./assets/nonLinearCantileverAero/cyilindricalCantBeam3D.gif" alt="plot check angular displacements" width="500"/>
+%md```
+%md
