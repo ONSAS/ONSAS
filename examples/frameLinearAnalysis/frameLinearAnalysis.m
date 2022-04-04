7da404c2733efe634f03ad2249ed183453b03d0d
jorgepz <jorgepz@fing.edu.uy>
2022-04-04 17:44:54 -0300

    working on torsion validation

--- examples/frameLinearAnalysis/frameLinearAnalysis.m
+++ examples/frameLinearAnalysis/frameLinearAnalysis.m
@@ -1,25 +1,25 @@
-%md# A Linear Beam Element Analysis Example
+%md# A Frame Linear Analysis Example
 %md
 %md## Previous definitions
 close all, clear all;
 addpath( genpath( [ pwd '/../../src' ] ) ) ; % add ONSAS directory to path
 %md
 %md scalar auxiliar parameters
-E = 210e6 ; L = 5 ; nu = 0.3 ;
-b = 0.3 ; % cross-section svgwidth
-P = 5  ; % applied nodal load
+E  = 210e6 ; nu = 0.3 ; %
+ty = 0.3   ; tz = 0.6 ; % cross-section widths
+L = 2      ;            %
+P = 1e3    ;            % applied nodal load
 %md
 %md## MEBI parameters: Material-Element-BoundaryConditions-InitialConditions
 %md
 %md### Materials
-materials.hyperElasModel = 'linearElastic' ;
-materials.hyperElasParams = [ E, nu] ;
+materials(1).hyperElasModel = 'linearElastic' ;
+materials(1).hyperElasParams = [ E, nu] ;
 %md### Elements
 elements(1).elemType  = 'node'  ;
 elements(2).elemType  = 'frame' ;
-elements(2).elemCrossSecParams{1,1} = 'rectangle' ;
-elements(2).elemCrossSecParams{2,1} = [2 b b ]    ;
-elements(2).elemTypeParams   =  1 ;
+elements(2).elemCrossSecParams = { 'rectangle'; [ ty tz ] };
+elements(2).elemTypeParams     =  1 ; % consistenMatrix
 %md### BoundaryConditions
 % Supports
 boundaryConds(1).imposDispDofs = [ 1 2 3 4 5 6 ] ;
@@ -34,28 +34,29 @@ initialConds = struct() ;
 %md## Mesh
 %md Mesh nodes
 mesh.nodesCoords = ...
-					[ 0 		0	 0		; ...
-						L   	0  0 	; ...
-						2*L   0  0 	; ...
-						2*L   0  L 	] ;
+					[ 0 	0	 0	; ...
+						L   0  0 	; ...
+						L   L  0 	] ;
+%md
 %md Conec Cell
 mesh.conecCell = { } ;
-%md Node elements. It is important to observe that only nodes with a boundary condition assigned are required to be included in the connectivity cell
-mesh.conecCell{1,1} = [ 0 1 1 0 1 ] ;
-mesh.conecCell{2,1} = [ 0 1 2 0 4 ] ;
-%md Frame elements
-mesh.conecCell{3,1} = [ 1 2 0 0   1 2 ] ;
-mesh.conecCell{4,1} = [ 1 2 0 0   2 3 ] ;
-mesh.conecCell{5,1} = [ 1 2 0 0   3 4 ] ;
-
-% Analysis settings
+%md nodes
+mesh.conecCell{1, 1 } = [ 0 1 1 0  1 ] ;
+mesh.conecCell{2, 1 } = [ 0 1 2 0  3 ] ;
+%md and frame elements
+mesh.conecCell{3, 1 } = [ 1 2 0 0  1 2 ] ;
+mesh.conecCell{4, 1 } = [ 1 2 0 0  2 3 ] ;
+%md
+%md Analysis settings
 analysisSettings = struct() ;
 
 otherParams.problemName = 'linearBeamElement' ;
-%otherParams.plotsFormat = 'vtk' ;
+otherParams.plotsFormat = 'vtk' ;
 
 [matUs, loadFactorsMat] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
 
+matUs
+stop
 %md## Verification
 A = b^2 ;  I = b^4/12 ;
 axial = E*A/L *2;  bending = E*I/L^3 *4*L^2*2 ;
