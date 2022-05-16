67335f86f08ee3e2c512e714c17ea68c13ab2afe
jorgepz <jorgepz@fing.edu.uy>
2022-05-16 20:52:57 -0300

    update headers

--- src/assembler.m
+++ src/assembler.m
@@ -1,20 +1,21 @@
-% Copyright 2022, Jorge M. Perez Zerpa, J. Bruno Bazzano, Joaquin Viera, 
-% Mauricio Vanzulli, Marcelo Forets, Jean-Marc Battini. 
-
-% This file is part of ONSAS 
-
+% Copyright 2022, Jorge M. Perez Zerpa, Mauricio Vanzulli, J. Bruno Bazzano,
+% Joaquin Viera, Marcelo Forets, Jean-Marc Battini. 
+%
+% This file is part of ONSAS.
+%
 % ONSAS is free software: you can redistribute it and/or modify 
 % it under the terms of the GNU General Public License as published by 
 % the Free Software Foundation, either version 3 of the License, or 
 % (at your option) any later version. 
-
+%
 % ONSAS is distributed in the hope that it will be useful, 
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-
+%
 % You should have received a copy of the GNU General Public License
 % along with ONSAS.  If not, see <https://www.gnu.org/licenses/>.
+
 %mdThis function computes the assembled force vectors, tangent matrices and stress matrices.
 function [ fsCell, stressMat, tangMatsCell ] = assembler ( Conec, elements, Nodes,...
                                                            materials, KS, Ut, Udott, Udotdott,...
--- updateLicenseHeaders.m
+++ updateLicenseHeaders.m
@@ -21,14 +21,16 @@ end
 
 function showHeaderAndReplace( filename )
 
-lengthOfCurrentHeader = '17';
+lengthOfCurrentHeader = 17 ;
 
-system( [ 'head -n ' lengthOfCurrentHeader ' ' filename ] );
+system( [ 'head -n ' num2str(lengthOfCurrentHeader) ' ' filename ] );
 
 reply = input('    replace yes or no? (y/n):','s' ) ;
 
 if strcmp( reply, 'y')
-  system( [ 'more currentFileHeader.txt > test.txt' ] ) ;
-  system( [ 'tail  -n ' lengthOfCurrentHeader ' ' filename ' >> test.txt'] ) ;
+  system( [ 'more currentFileHeader.txt > aux.txt' ] ) ;
+  system( [ 'tail  -n +' num2str(lengthOfCurrentHeader+1) ' ' filename ' >> aux.txt'] ) ;
+  system( [ 'mv aux.txt ' filename ] ) ;
+  disp('file updated.')
 end
 pause
