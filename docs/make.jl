9aeb0c1707b97ffc0b8344f6137aa0b3ac88b572
jorgepz <jorgepz@fing.edu.uy>
2022-05-20 17:33:02 -0300

    remove inclusion example from docs

--- docs/make.jl
+++ docs/make.jl
@@ -34,7 +34,6 @@ elseif outputFormat == "html"
                                      "Spring-mass system" => "examples/springMass.md",
                                     "Uniaxial extension" => "examples/uniaxialExtension.md",
                                     "Cantilever Beam" => "examples/cantileverBeam.md",
-                                    "Solid with inclusion" => "examples/semiSphereWithInclusion.md",
                                     "Linear aerodynamics" => "examples/linearAerodynamics.md",
                                     "Non-linear aerodynamics" => "examples/nonLinearAerodynamics.md"],
       "User guide" => Any["Installation" =>  "howtouse/install.md",
@@ -52,13 +51,3 @@ elseif outputFormat == "html"
   )
 
 end
-#
-
-#         "Guide" => Any["Installation"     => "howtouse/install.md",
-#         "Theory" => Any["Virtual mechanical work " => "theory/prinMechWork.md",
-#                         "References"               => "theory/references.md"  ],
-#         "About" => "about.md",
-#     ],
-#     strict = false
-# )
-#
