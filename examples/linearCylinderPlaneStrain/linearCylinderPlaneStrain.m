7f536ff5ad1d7fe8d460979ad96de5cf9dd8ceed
mvanzulli <mvanzulli@fing.edu.uy>
2022-05-20 20:46:02 -0300

    solving pressure bug and jorge comments

--- examples/linearCylinderPlaneStrain/linearCylinderPlaneStrain.m
+++ examples/linearCylinderPlaneStrain/linearCylinderPlaneStrain.m
@@ -2,16 +2,17 @@
 %md
 %md[![Octave script](https://img.shields.io/badge/script-url-blue)](https://github.com/ONSAS/ONSAS.m/blob/master/examples/linearPlaneStrain/onsasExample_linearPlaneStrain.m)
 %md
-%md In this example a hollow cylinder submitted to an internal pressure $p_i$ Pa as shown in the diagram depicted below is considered. The length of the cylinder is $L_z $ m and the internal and external radious are $R_i$ m and $R_e$ m, respectively. 
+%md In this example a hollow cylinder submitted to an internal pressure $p_i$ as shown in diagram depicted below is considered. The length of the cylinder is $L_z $ m and the internal and external radious are $R_i$ and $R_e$, respectively. 
 %md
 %md```@raw html
 %md<img src="../../assets/linearCylinderPlaneStrain/ilusCylinderPlaneStrain.svg" alt="linear cylinder diagram" width="500"/>
 %md```
 %md
 %md A cylindrical system of coordinates is defined considering the unitary vectors ($e_r$, $e_\theta$, $e_z$). 
-%md The material employed is isotropic and homogeneous with elasticity modulus $E=1$ MPa and Poisson's ratio $\nu=0.3$. The boundary condition on the Cauchy stress tensor $\mathbf{\sigma}$ is given by the radial pressure on the internal surface:
+%md The material employed is isotropic and homogeneous with elasticity modulus $E$ MPa and Poisson's ratio $\nu$. The boundary condition on the stress tensor $\textbf{T}$ is given by the radial pressure on the internal surface:
 %md```math 
-%md \mathbf{\sigma}(Ri, \theta, z).e_r=p_i\;\;\quad\forall\; (\theta,z) \\
+%md\textbf{T}(r=R_i, \theta, z)\left[-e_r\right] =p_i e_r\;\;\quad\forall\; (\theta,z) \\
+%md\textbf{T}(r=R_e, \theta, z)\left[e_r\right]=\textbf{0} \;\;\quad\forall\; (\theta,z)
 %md```
 %md and the plane strain boundary conditions imposed to the displacement field are:
 %md```math 
@@ -21,7 +22,7 @@
 %md
 %md## Analytic solution
 %md
-%md The solution displacement field is obtained using the Navier's equation, imposing no temperature variation and no volumetric forces field. Thus the displacements field $\mathbf{\mathit{u}}$ solution satisfies:
+%md The solution displacement field is extracted from chapter 4 of  (Timoshenko and Goodier, Theory of Elasticity, 3rd edition). The Navier's equation, imposing no temperature variation, no volumetric forces, and considering a radial dispalcement field leads to:
 %md```math
 %md  \nabla (\nabla . \textbf{u}(r,\theta,z) )  = 0  
 %md```
@@ -37,7 +38,7 @@ clear all, close all
 % add path
 addpath( genpath( [ pwd '/../../src'] ) ) ;
 % scalar parameters
-E = 1e6 ; nu = 0.3 ; p = 1 ; L = .75 ;
+E = 1e6 ; nu = 0.3 ; p = 30e3 ; L = .75 ; Re = 0.15 ; Ri = 0.1 ;
 %md
 %md
 %md### MEBI parameters
@@ -51,25 +52,25 @@ materials.hyperElasParams =  [ E nu ]       ;
 %md#### elements
 %md 
 %md In this plane model, three kinds of elements are used: `triangle` for the solid, `edges` to add pressure loads and `nodes` to set additional boundary conditions for the numerical resolution. Since three kinds of elements are used, the struct has length 3: 
-elements(1).elemType = 'node';
-elements(2).elemType = 'edge';
-elements(2).elemCrossSecParams = L ;
-elements(3).elemType = 'triangle';
-elements(3).elemTypeParams = 2 ;
-elements(3).elemCrossSecParams = L ;
+elements(1).elemType           = 'node'    ;
+elements(2).elemType           = 'edge'    ;
+elements(2).elemCrossSecParams = L         ;
+elements(3).elemType           = 'triangle';
+elements(3).elemTypeParams     = 2         ;
+elements(3).elemCrossSecParams = L         ;
 %md where `elemCrossSecParams` field sets the thickness of the edge and `elemTypeParams` sets the plane strain triangle element.  
 %md
 %md#### boundaryConds
 %md Three BCs are considered, one corresponding to a load and two for displacements.
-%md The first two BCs constrain displacements in $x$ and $y$ local directions respectively:
+%md The first two BCs constrain displacements in $x$ and $y$ global directions respectively:
 boundaryConds(1).imposDispDofs = [1] ;
 boundaryConds(1).imposDispVals = [0] ;
 boundaryConds(2).imposDispDofs = [3] ;
 boundaryConds(2).imposDispVals = [0] ;
-%md then the third BC corresponds to the pressure. It is introduced in `local` coordinates:
+%md then the third BC corresponds to the pressure. It is introduced in `local` coordinates so the first entry is along the edge (tangent) and the second towards the normal vector obtained by rotating the tangent vector 90 degrees in global axis $z$:
 boundaryConds(3).loadsCoordSys = 'local' ;
 boundaryConds(3).loadsTimeFact = @(t) t  ;
-boundaryConds(3).loadsBaseVals = [ p 0  0 0  0 0 ]  ;
+boundaryConds(3).loadsBaseVals = [ 0 p ]  ;
 %md
 %md#### initialConds
 %md Any non-homogeneous initial conditions are considered, thereafter an empty struct is set:
@@ -81,8 +82,9 @@ initialConds = struct();
 %md```@raw html
 %md<img src="../../assets/linearCylinderPlaneStrain/meshCylinderPlaneStrain.png" alt="mesh plot" width="500"/>
 %md```
-%md
-%md the mesh is read using the `meshFileReader.m` function
+%md The element properties are set using labels into GMSH follwing the MEBI nomenclature. First `triangle` elements have linear elastic material so entry $1$ of the _materialṣ_ struct is assigned. Then for both `node` and `edge` elements any material is set. 
+%md Next displacement boundary conditions are assigned to the element, since the problem is modeled into $x-y$ plane, a constrain to avoid rotation along $z$ is necessary. This is done fixing $y$ and $x$ displacements (using `boundaryConds(1)` and `boundaryConds(2)` as labels) on points 2 3 4 5.
+%md Finally the internal pressure is applied on the `edge` elements linked with curves from one to four (Circles 1-4 in Figure). In accordance with the orientation of the curve set in GMSH, the normal vector obtained in local coordinates is $e_r$ so the internal pressure is assigned using `boundaryConds(3)`. Once the mesh is created is read using:
 [ mesh.nodesCoords, mesh.conecCell ] = meshFileReader( 'ring.msh' ) ;
 %md
 %md### Analysis parameters
@@ -105,23 +107,22 @@ otherParams.plotsFormat = 'vtk' ;
 %md
 %md## Verification
 %mdThe numerical and analytic solutions are compared at the final load step for the internal and external surface (since all the elements on the same surface have the same analytic solution):
-Rext = 0.15 ; Rint = 0.1 ;
 % internal surface analytic solution
-A = ( p * (1+nu)*(1-2*nu)*Rint^2 ) / ( E*(Rext^2-Rint^2) ) ;
-B = ( p * (1+nu)*Rint^2*Rext^2 )   / ( E*(Rext^2-Rint^2) ) ;
-analyticValRInt = A*Rint + B/Rint ;
+A = ( p * (1+nu)*(1-2*nu)*Ri^2 ) / ( E*(Re^2-Ri^2) ) ;
+B = ( p * (1+nu)*Ri^2*Re^2 )   / ( E*(Re^2-Ri^2) ) ;
+analyticValRi = A*Ri + B/Ri ;
 % internal surface numerical solution
-dofXRint = 1 ;
-numericalRInt = matUs( dofXRint, end ) ;
+dofXRi = 1 ;
+numericalRi = matUs( dofXRi, end ) ;
 % external surface analytic solution
-analyticValRExt = A*Rext + B/Rext ;
+analyticValRe = A*Re + B/Re ;
 % external surface numerical solution
-dofXRext = (8-1)*6+3 ;
-numericalRExt = matUs( dofXRext , end ) ;
+dofXRe = (8-1)*6+3 ;
+numericalRe = matUs( dofXRe , end ) ;
 %md The numerical solution is verified: 
 analyticCheckTolerance = 1e-3 ;
-verifBoolean = ( ( numericalRInt - analyticValRInt ) < analyticCheckTolerance ) && ...
-               ( ( numericalRExt - analyticValRExt ) < analyticCheckTolerance )
+verifBoolean = ( ( numericalRi - analyticValRi ) < analyticCheckTolerance ) && ...
+               ( ( numericalRe - analyticValRe ) < analyticCheckTolerance )
 %md
 %md### Plot
 %md
@@ -130,11 +131,11 @@ verifBoolean = ( ( numericalRInt - analyticValRInt ) < analyticCheckTolerance ) 
 lw = 2.0 ; ms = 11 ; plotfontsize = 10 ;
 figure, hold on, grid on
 %internal surface
-plot( matUs(dofXRint,:), loadFactorsMat(:,3) , 'ro' , 'linewidth', lw,'markersize',ms )
-plot( linspace(0,analyticValRInt,length(loadFactorsMat(:,3) ) )  , loadFactorsMat(:,3), 'k-', 'linewidth', lw,'markersize',ms )
+plot( matUs(dofXRi,:), loadFactorsMat(:,3) , 'ro' , 'linewidth', lw,'markersize',ms )
+plot( linspace(0,analyticValRi,length(loadFactorsMat(:,3) ) )  , loadFactorsMat(:,3), 'k-', 'linewidth', lw,'markersize',ms )
 %internal surface
-plot( matUs(dofXRext,:), loadFactorsMat(:,3) , 'ro' , 'linewidth', lw,'markersize',ms )
-plot( linspace(0,analyticValRExt,length(loadFactorsMat(:,3)) ) , loadFactorsMat(:,3), 'k-', 'linewidth', lw,'markersize',ms )
+plot( matUs(dofXRe,:), loadFactorsMat(:,3) , 'ro' , 'linewidth', lw,'markersize',ms )
+plot( linspace(0,analyticValRe,length(loadFactorsMat(:,3)) ) , loadFactorsMat(:,3), 'k-', 'linewidth', lw,'markersize',ms )
 labx = xlabel('Displacement [m]');   laby = ylabel('\lambda(t)') ;
 legend('Numeric','Analytic','location','East')
 set(gca, 'linewidth', 1.2, 'fontsize', plotfontsize )
