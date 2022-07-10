334c66d6b62e93c1b2bfc03695eee506fa7fba99
jorgepz <jorgepz@fing.edu.uy>
2022-07-10 19:20:36 -0300

    add working test for GIP

--- test/gaussIntegrationTest.m
+++ test/gaussIntegrationTest.m
@@ -1,42 +1,43 @@
 
 function resultBoolean = gaussIntegrationTest()
 
-  point_nums_to_test = [ 1:10 ]
+  plots_boolean = false ;
+
+  point_nums_to_test = [ 1:10 12:14 16 26   ]
+%  point_nums_to_test = [ 1:10    ]
   int_values = zeros( size(point_nums_to_test) ) ;
 
   a = -1.5 ;  b =  1 ;
-  analyInt = -50*.5 + -50*1*-5 + 100*.5*.5 
+  analyInt = -50*.5 + -50*1*.5 + 100*.5*.5 + 100*.5  ;
 
   xs = -1.5:.02:1;
   ys = 0;
 
   for i=1:length(xs)
-    ys(i) = fun_to_integrate(xs(i));
+    ys(i) = test_fun_to_integrate(xs(i));
   end
 
-  plot( xs, ys )
+  if plots_boolean
+    figure
+    plot( xs, ys, 'g-x' )
+    title('test_fun_to_integrate')
+  end
 
   for j=1:length( point_nums_to_test)
     [xIntPoints, wIntPoints] = GaussPointsAndWeights ( point_nums_to_test(j) ) ;
 
     for k=1:length(xIntPoints)
       int_values(j) = int_values(j) + ...
-                      wIntPoints(k) * fun_to_integrate( (b-a)/2 * xIntPoints(k) + (a+b)/2 ) * ( (b-a) /2 ) ;
+                      wIntPoints(k) * test_fun_to_integrate( (b-a)/2 * xIntPoints(k) + (a+b)/2 ) * ( (b-a) /2 ) ;
     end
   end
-
-  figure
-  plot(point_nums_to_test, int_values)
-
-resultBoolean = true
-function y = fun_to_integrate( x )
-
-  if x > 0.5
-    y = 0 ;
-  elseif x>0,
-    y = 200 * x ;
-  elseif x>-1,
-    y = 50 * x ;
-  else
-    y = -50 ;
+  
+  if plots_boolean
+    numericalInt = quadl( 'test_fun_to_integrate', a, b )
+    figure
+    plot(point_nums_to_test, int_values,'b-x')
+    hold on, grid on
+    plot(point_nums_to_test, numericalInt*ones(size(point_nums_to_test)),'r-o')
   end
+
+  resultBoolean = norm( int_values( end ) - analyInt ) / norm( analyInt ) < 1e-2 ;
---
+++ test/test_fun_to_integrate.m
@@ -0,0 +1,20 @@
+function ys = test_fun_to_integrate( xinput )
+
+ys = zeros( size(xinput) ) ;
+
+for j=1:length( xinput)
+  x = xinput(j);
+
+  if x > 0.5
+    y = 100 ;
+    %y = 0 ;
+  elseif x>0,
+    y = 200 * x ;
+  elseif x>-1,
+    y = 50 * x ;
+  else
+    y = -50 ;
+  end
+
+  ys(j) = y ;
+end
