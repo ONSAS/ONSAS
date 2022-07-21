39e75c73ae91f76a2966eaf023a5334cebbfca8b
TheChikenfly <100605156+TheChikenfly@users.noreply.github.com>
2022-07-21 13:48:16 -0400

    WOM functions

--- src/elements/frame/WOMV3.m
+++ src/elements/frame/WOMV3.m
@@ -1,14 +1,15 @@
-function qelem = WOMV2(vprvect1, vprvect2, Udotdottp1k1, Udotdottp1k2,tl1, tl2, D, tnp1, dt)
-% Computes the value of q for the element
+function qelem = WOMV3(vprvect1, vprvect2, Udotdottp1k1, Udotdottp1k2,tl1, tl2, D, tnp1, dt,Kelem)
+% Computes the value of q for the element K 
 % In WOMV2 we solve only one VdP equation with the averages values of the
 % nodal vpr and Udotdot.
 % vpr1, vpr2: relative velocities at nodes 1 and 2 (vpr = Ucos(theta0))
-global qvect; % to be replaced by a xls file later
+global qvect; % To be removed!
 n = uint16(tnp1/dt - 1); % Current time
+K = Kelem-1;
 if n == 0 % First call
-    %create xlsfile or txt file
-    q01 = 2; q02 = 2; % Should be random number of magnitude O(0.001)
-    qvect = [(q01+q02)/2;0] ;  % qvect = [q0elem q1elem ...; dq0elem dq1elem ...;]
+    q01 = 0.001; q02 = 0.001; % Should be random number of magnitude O(0.001)
+    % Initialize qvect 
+    qvect(1+K*2:2+K*2,1) = [(q01+q02)/2;0] ;  % qvect = [q0elem q1elem ...; dq0elem dq1elem ...;]
     qelem = (q01+q02)/2;
 else
     vpr1 = norm(vprvect1);
@@ -20,23 +21,22 @@ else
     ddY2 = dot(Udotdottp1k2, tl2); % Ydotdott of node 2 at time n
     ddYelem = (ddY1 + ddY2)/2;
     % Calls computeq for the element
-    [qnp1elem dqnp1elem ] = computeq(ddYelem, D, tnp1, dt, vprelem, n);
+    qn = qvect(1+K*2,n);
+    dqn = qvect(2+K*2,n);
+    %[qnp1elem dqnp1elem ] = computeq(ddY2, D, tnp1, dt, vprelem, qn, dqn);
+    [qnp1elem dqnp1elem ] = computeq(ddYelem, D, tnp1, dt, vprelem, qn, dqn);
     qelem = qnp1elem;
     % Updating qvect
-    qvect(1:2, n+1) = [qnp1elem dqnp1elem];
+    qvect(1+K*2:2+K*2, n+1) = [qnp1elem dqnp1elem];
 end
 %function computing q at node i
-function [qnp1 dqnp1] = computeq(ddYelem, D, tnp1, dt, vprelem, n)% at node i= 1,2
+function [qnp1 dqnp1] = computeq(ddYelem, D, tnp1, dt, vprelem, qn, dqn)% at node i= 1,2
     %time increments
-    N= 5; % Number of steps
+    N= 2; % Number of steps
     h= dt/N; % Time step
     t = tnp1 - dt:h:tnp1; % Interval on which ode45 solves the VdP equation
-    % Initial parameters
-    qvect;
-    qn = qvect(1,n);
-    dqn = qvect(2,n);
     % WOM constants from Facchinetti et al
-    A = 12; epsilon = 0.3; St = 0.16;
+    A = 12; epsilon = 0.3; St = 0.2;%0.16;
     % VdP oscillator constants
     B = 2*pi*St*vprelem/D;
     cq = epsilon*B;
