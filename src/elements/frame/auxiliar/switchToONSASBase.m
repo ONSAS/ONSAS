de127d9baa2dfa1506b2d3921b5db22ae35547d5
mvanzulli <mvanzulli@fing.edu.uy>
2022-08-24 22:40:09 -0300

    fixing pipeline

--- src/elements/frame/auxiliar/switchToONSASBase.m
+++ src/elements/frame/auxiliar/switchToONSASBase.m
@@ -16,14 +16,22 @@
 % You should have received a copy of the GNU General Public License
 % along with ONSAS.  If not, see <https://www.gnu.org/licenses/>.
 
-function B = switchToBattiniNom(O)
+function B = switchToONSASBaseMat(A)
+
 %---------------- Change of basis Le and Battini 2014 -> ONSAS matrix  -------------------
-  permutIndxs = [1:2:5 2:2:6 ([1:2:5]+6) ([2:2:6]+6) ] ;
+Pch = sparse (6,6);
+Pch (1,1) = 1;
+Pch (3,2) = 1;
+Pch (5,3) = 1;
+Pch (2,4) = 1;
+Pch (4,5) = 1;
+Pch (6,6) = 1;
 
-	if size(O,2)>1
-		B = O(permutIndxs, permutIndxs)
-		else
-		B = O(permutIndxs);
+P = [Pch sparse(6,6);sparse(6,6) Pch];
+
+    if size(A,2)>1
+		B = P*A*P';
+	 else
+		B = P*A;
 	end
-
 end
