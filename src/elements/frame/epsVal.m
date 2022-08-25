ee1a3ce2173e861659c9b7bcb9e8685e045dedc6
joaquinv <joaquinv@fing.edu.uy>
2022-08-25 12:47:17 -0300

    agrego cambio

--- src/elements/frame/epsVal.m
+++ src/elements/frame/epsVal.m
@@ -2,11 +2,4 @@ function epsk = epsVal(z, B, R, Ut)
 
 	epsk = -z'.* B * R * Ut ;
 
-
-	%~ eps_sup = -0.00251990926724482 ;
-	%~ tz = 0.3 ;
-	
-	%~ epsk = -2*eps_sup / tz * z' + eps_sup ;
-	
-	
 end
