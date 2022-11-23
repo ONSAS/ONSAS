e22424b5fc1498d02c4c2b91b4ceed5dab377f52
TheChikenfly <100605156+TheChikenfly@users.noreply.github.com>
2022-11-23 16:22:57 -0500

    update

--- src/core/system_assembler.m
+++ src/core/system_assembler.m
@@ -35,7 +35,6 @@ function [systemDeltauMatrix, systemDeltauRHS, FextG, fs, nexTimeLoadFactors ] =
     global exportFirstMatrices;
     if exportFirstMatrices == true
       KTred      = KT( neumdofs, neumdofs );
-      neumdofs
       massMatred = massMat(neumdofs,neumdofs);
       save('-mat', 'output/matrices.mat', 'KT','massMat','neumdofs' );
       figure
