949b7855a0c2a8bdaa554d8c72530f63c4b20a43
Mauricio Vanzulli <50339940+mvanzulli@users.noreply.github.com>
2023-02-04 11:38:09 -0300

    Update examples/uniaxialCompression/uniaxialCompression.m
    Co-authored-by: Jorge Pérez Zerpa <42485529+jorgepz@users.noreply.github.com>

--- examples/uniaxialCompression/uniaxialCompression.m
+++ examples/uniaxialCompression/uniaxialCompression.m
@@ -23,7 +23,7 @@
 %mdThe neo-Hookean elastic strain energy potential $\Psi$ is given by:
 %md```math
 %md\Psi(\mathbf{C=\textbf{F}^T\textbf{F}}) = \frac{\mu}{2}(I_1 -3) + K ( J^2 -1 )
-%md```math
+%md```
 %mdwhere $I_1 = \mathrm{tr}(\mathbf{C})$ is the first invariant, $J = \sqrt{\det(\mathbf{C})}$ and $K$ and $\mu$ are the bulk and shear material parameters, respectively.
 %md
 %mdThe second Piola-Kirchhoff tensor is given by:
