af6013047fdb0aaa7b3c5cdcb107e2aefb009c7f
jorgepz <jorgepz@fing.edu.uy>
2023-02-11 10:19:00 -0300

    renaming files

--- test/runTestProblems_moxunit_disp.m
+++ test/runTestProblems_moxunit_disp.m
@@ -8,72 +8,70 @@ function test_suite=runTestProblems_moxunit_disp
   end
   initTestSuite;
 
+
+
 function test_1
-  static_von_mises_truss
-  assertEqual( verifBoolean, true );
-
-function test_2
-  uniformCurvatureCantilever
-  assertEqual( verifBoolean, true );
-
-function test_3
-  linearCylinderPlaneStrain
-  assertEqual( verifBoolean, true );
-
-function test_4
-  uniaxialExtension
-  assertEqual( verifBoolean, true );
-
-function test_5
-  nonlinearPendulum
-  assertEqual( verifBoolean, true );
-
-function test_6
-  springMass
-  assertEqual( verifBoolean, true );
-
-function test_7
-  onsasExample_cantileverSelfWeight
-  assertEqual( verifBoolean, true );
-
-function test_8
-  simplePropeller
-  assertEqual( verifBoolean, true );
-
-function test_9
-  frameLinearAnalysis
-  assertEqual( verifBoolean, true );
-
-function test_10
-  linearAerodynamics
-  assertEqual( verifBoolean, true );
-
-function test_11
   beamLinearVibration
   assertEqual( verifBoolean, true );
 
+function test_2
+	cantilever_ModalAnalysis
+  assertEqual( verifBoolean, true );
+
+function test_3
+  cantileverSelfWeight
+  assertEqual( verifBoolean, true );
+
+function test_4
+  dragBeamReconfiguration
+  assertEqual( verifBoolean, true );
+
+function test_5
+	eulerColumn
+  assertEqual( verifBoolean, true );
+
+function test_6
+  frameLinearAnalysis
+  assertEqual( verifBoolean, true );
+
+function test_7
+  linearAerodynamics
+  assertEqual( verifBoolean, true );
+
+function test_8
+  linearCylinderPlaneStrain
+  assertEqual( verifBoolean, true );
+
+function test_9
+  nonlinearPendulum
+  assertEqual( verifBoolean, true );
+
+function test_10
+  springMass
+  assertEqual( verifBoolean, true );
+
+function test_11
+  simplePropeller
+  assertEqual( verifBoolean, true );
+
 function test_12
-  ONSAS_VIVtest
+  staticVonMisesTruss
   assertEqual( verifBoolean, true );
 
 function test_13
-  circularReconfiguration
+  uniaxialCompression
   assertEqual( verifBoolean, true );
 
 function test_14
-	cantileverLinearHardening
+  uniaxialExtension
   assertEqual( verifBoolean, true);
 
 function test_15
-  	eulerColumn
-    assertEqual( verifBoolean, true);
+  uniformCurvatureCantilever
+  assertEqual( verifBoolean, true);
 
 function test_16
-  	cantilever_modal_analysis
-    assertEqual( verifBoolean, true);
-
-function test_17
-  uniaxialCompression
+  VIVCantilever
   assertEqual( verifBoolean, true );
 
 function test_18
