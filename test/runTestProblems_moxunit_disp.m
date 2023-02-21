86edb6e9fb5aef46cf9c4a0e7fa5a433def37399
jorgepz <jorgepz@fing.edu.uy>
2023-02-21 19:01:48 -0300

    debugging

--- test/runTestProblems_moxunit_disp.m
+++ test/runTestProblems_moxunit_disp.m
@@ -12,65 +12,65 @@ function test_1
   beamLinearVibration
   assertEqual( verifBoolean, true );
 
-function test_2
-	cantileverModalAnalysis
-  assertEqual( verifBoolean, true );
+% function test_2
+% 	cantileverModalAnalysis
+%   assertEqual( verifBoolean, true );
 
-function test_3
-  cantileverSelfWeight
-  assertEqual( verifBoolean, true );
+% function test_3
+%   cantileverSelfWeight
+%   assertEqual( verifBoolean, true );
 
-function test_4
-  dragBeamReconfiguration
-  assertEqual( verifBoolean, true );
+% function test_4
+%   dragBeamReconfiguration
+%   assertEqual( verifBoolean, true );
 
-function test_5
-	eulerColumn
-  assertEqual( verifBoolean, true );
+% function test_5
+% 	eulerColumn
+%   assertEqual( verifBoolean, true );
 
-function test_6
-  frameLinearAnalysis
-  assertEqual( verifBoolean, true );
+% function test_6
+%   frameLinearAnalysis
+%   assertEqual( verifBoolean, true );
 
-function test_7
-  linearAerodynamics
-  assertEqual( verifBoolean, true );
+% function test_7
+%   linearAerodynamics
+%   assertEqual( verifBoolean, true );
 
-function test_8
-  linearCylinderPlaneStrain
-  assertEqual( verifBoolean, true );
+% function test_8
+%   linearCylinderPlaneStrain
+%   assertEqual( verifBoolean, true );
 
-function test_9
-  nonlinearPendulum
-  assertEqual( verifBoolean, true );
+% function test_9
+%   nonlinearPendulum
+%   assertEqual( verifBoolean, true );
 
-function test_10
-  springMass
-  assertEqual( verifBoolean, true );
+% function test_10
+%   springMass
+%   assertEqual( verifBoolean, true );
 
-function test_11
-  simplePropeller
-  assertEqual( verifBoolean, true );
+% function test_11
+%   simplePropeller
+%   assertEqual( verifBoolean, true );
 
-function test_12
-  staticVonMisesTruss
-  assertEqual( verifBoolean, true );
+% function test_12
+%   staticVonMisesTruss
+%   assertEqual( verifBoolean, true );
 
-function test_13
-  uniaxialCompression
-  assertEqual( verifBoolean, true );
+% function test_13
+%   uniaxialCompression
+%   assertEqual( verifBoolean, true );
 
-function test_14
-  uniaxialExtension
-  assertEqual( verifBoolean, true);
+% function test_14
+%   uniaxialExtension
+%   assertEqual( verifBoolean, true);
 
-function test_15
-  uniformCurvatureCantilever
-  assertEqual( verifBoolean, true);
+% function test_15
+%   uniformCurvatureCantilever
+%   assertEqual( verifBoolean, true);
 
-function test_16
-  VIVCantilever
-  assertEqual( verifBoolean, true );
+% function test_16
+%   VIVCantilever
+%   assertEqual( verifBoolean, true );
 
-function test_17
-  assertEqual( gaussIntegrationTest, true);
+% function test_17
+%   assertEqual( gaussIntegrationTest, true);
