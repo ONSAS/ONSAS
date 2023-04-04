6808995ab9246cdb2b963af1beb46e9b4d2ffda7
Joaquin Viera <45524519+JoaquinViera@users.noreply.github.com>
2023-04-04 19:16:07 -0300

    Update src/core/ONSAS_init.m
    Co-authored-by: Jorge Pérez Zerpa <42485529+jorgepz@users.noreply.github.com>

--- src/core/ONSAS_init.m
+++ src/core/ONSAS_init.m
@@ -71,7 +71,7 @@ nNodes = size( mesh.nodesCoords, 1 ) ;
 [ U, Udot, Udotdot ] = initialCondsProcessing( initialConds, nNodes ) ;
 
 %~ previous_state_mat = zeros( size(Conec,1), 3 ) ; % assumed only for trusses: scalar per element
-previous_state_mat = cell( size(Conec,1), 3) ;
+previousStateCell = cell( size(Conec,1), 3) ;
 previous_state_mat(:,1) = {zeros( 1, 3 )} ;
 previous_state_mat(:,2) = {zeros( 1, 3 )} ;
 previous_state_mat(:,3) = {0} ;
