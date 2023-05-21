3d790c4b12f4f32c5b8eaa1f981f00824a8590d2
Sergio A. Merlino Chiozza <ing.merlino@gmail.com>
2023-05-21 18:03:20 -0300

    +

--- examples/Modelos Tesis/classtruss3bar.m
+++ examples/Modelos Tesis/classtruss3bar.m
@@ -1,22 +1,21 @@
-%md# Classical Truss Three Rods
-
-close all; clear; addpath( genpath( [ pwd '/../../src'] ) );
-format long;
+%md# classical truss / 3 rods
+close all; clear;
+addpath( genpath( [ pwd '/../../src'] ) );
 % scalar parameters (N/cm2)
 E = 210e3 ;
 Kplas = 1093 ;
 sigma_Y_0 = 330.3 ;
 Fu = 1 ;
 
-global Rstress Rstrain Rstrainacum Largo ;
-
 % x and z coordinates of node 4
 x2 = 300 ;
 z2 = -400 ;
 
+materials = struct();
 materials.hyperElasModel  = 'isotropicHardening' ;
 materials.hyperElasParams = [ E Kplas sigma_Y_0 ] ;
 
+elements = struct();
 elements(1).elemType = 'node' ;
 elements(2).elemType = 'truss';
 elements(3).elemType = 'truss';
@@ -25,78 +24,72 @@ elements(2).elemCrossSecParams = { 'circle' , sqrt(1*4/pi)} ;
 elements(3).elemCrossSecParams = { 'circle' , sqrt(1*4/pi)} ;
 elements(4).elemCrossSecParams = { 'circle' , sqrt(1*4/pi)} ;
 
+boundaryConds = struct();
 boundaryConds(1).imposDispDofs = [ 1 3 5 ] ;
 boundaryConds(1).imposDispVals = [ 0 0 0 ] ;
 boundaryConds(2).imposDispDofs = 3 ;
 boundaryConds(2).imposDispVals = 0 ;
 boundaryConds(2).loadsCoordSys = 'global' ;
 boundaryConds(2).loadsTimeFact = @(t) (t<=1)*(Fu)*t ;
-boundaryConds(2).loadsBaseVals = [ 0 0 0 0 1 0 ]  ;
+boundaryConds(2).loadsBaseVals = [ 0 0 0 0 -1 0 ]   ;
 
-initialConds = struct() ;
-
-mesh.nodesCoords = [   0  0   0 ; ...
-                      x2  0   0 ; ...
-                    2*x2  0   0 ; ...
-                      x2  0   z2 ] ;
+mesh = struct();
+mesh.nodesCoords = [   0  0   0     ; ...
+                      x2  0   0     ; ...
+                    2*x2  0   0     ; ...
+                      x2  0   z2 ]  ;
 
 % MEBI [Material Element Boundary_Conditions Initial_Conditions]
 
 mesh.conecCell = cell(7,1) ;
-mesh.conecCell{ 1, 1 } = [ 0 1 1 0  1   ] ;
-mesh.conecCell{ 2, 1 } = [ 0 1 1 0  2   ] ;
-mesh.conecCell{ 3, 1 } = [ 0 1 1 0  3   ] ;
-mesh.conecCell{ 4, 1 } = [ 0 1 2 0  4   ] ;
-mesh.conecCell{ 5, 1 } = [ 1 2 0 0  1 4 ] ;
-mesh.conecCell{ 6, 1 } = [ 1 3 0 0  2 4 ] ;
-mesh.conecCell{ 7, 1 } = [ 1 4 0 0  3 4 ] ;
+mesh.conecCell{ 1, 1 } = [ 0 1 1  1   ] ;
+mesh.conecCell{ 2, 1 } = [ 0 1 1  2   ] ;
+mesh.conecCell{ 3, 1 } = [ 0 1 1  3   ] ;
+mesh.conecCell{ 4, 1 } = [ 0 1 2  4   ] ;
+mesh.conecCell{ 5, 1 } = [ 1 2 0  1 4 ] ;
+mesh.conecCell{ 6, 1 } = [ 1 3 0  2 4 ] ;
+mesh.conecCell{ 7, 1 } = [ 1 4 0  3 4 ] ;
 
-analysisSettings.deltaT        =      1 ;
-analysisSettings.finalTime     =    100 ;
+initialConds                = struct() ;
+
+analysisSettings = struct();
+analysisSettings.deltaT        =   1    ;
+analysisSettings.finalTime     =   100  ;
+
 analysisSettings.stopTolDeltau =   1e-8 ;
 analysisSettings.stopTolForces =   1e-8 ;
 analysisSettings.stopTolIts    =   15   ;
 
-otherParams.problemName       = 'ClassicalTruss_log_strain_arcLength' ;
-analysisSettings.methodName   = 'arcLength' ;
-
-analysisSettings.incremArcLen = 0.064;
-analysisSettings.iniDeltaLamb = boundaryConds(2).loadsTimeFact(1)/100 ;
 analysisSettings.posVariableLoadBC = 2 ;
 
-otherParams.problemName = 'classtruss' ;
+otherParams = struct();
 otherParams.plots_format = 'vtk' ;
 otherParams.plots_deltaTs_separation = 2 ;
 
-[ matUs, loadFactorsMat ] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
-
-deltas = [-matUs(6*3+1,:)' -matUs(6*3+5,:)'] ;
-
-  x1=zeros(floor(analysisSettings.finalTime/analysisSettings.deltaT),1);
-  y1=zeros(floor(analysisSettings.finalTime/analysisSettings.deltaT),1);
-  F=zeros(floor(analysisSettings.finalTime/analysisSettings.deltaT),1);
-    for t = 1:(floor(analysisSettings.finalTime/analysisSettings.deltaT))
-        x1(t)=Rstrain(t,1);
-        y1(t)=Rstress(t,1);
-        % Fuerza aplicada vertical
-        F(t)=(z2-deltas(t,2))./Largo(t,2).*1.*y1(t);
-    end
-    x2=zeros(floor(analysisSettings.finalTime/analysisSettings.deltaT),1);
-    y2=zeros(floor(analysisSettings.finalTime/analysisSettings.deltaT),1);
-    for t = 1:(floor(analysisSettings.finalTime/analysisSettings.deltaT))
-        x2(t)=Rstrain(t,2);
-        y2(t)=Rstress(t,2);
-    end
-
-        figure(1);
-        plot(-deltas(2:end,2),F,'LineWidth',1.5);
-        title('Plasticidad / Barras --ONSAS-- F_{apl}(\delta)');
-        ylabel('Fuerza Vertical F_{apl}');
-        xlabel('Desplazamiento \delta');
-        hold on;
-
-fprintf('|Desplazamiento vertical\n');
-fprintf('|Solución numérica  %.15f\n',abs(deltas(length(deltas),2)));
-fprintf('|Deformación plástica acumulada (alfa) %.15f\n',Rstrainacum(1));
-
 % an Eternal Golden Braid
+
+otherParams.problemName       = 'staticVonMisesTruss_NRAL_Jirasek_Green'    ;
+analysisSettings.methodName   = 'arcLength'                                 ;
+analysisSettings.finalTime    = 100                                         ;
+analysisSettings.incremArcLen = [0.65*ones(1,100)]                          ;
+analysisSettings.iniDeltaLamb = boundaryConds(2).loadsTimeFact(.1)/100      ;
+analysisSettings.posVariableLoadBC = 2 ;
+
+global arcLengthFlag
+arcLengthFlag = 2 ;
+
+global dominantDofs
+dominantDofs = 6*3+5 ;
+
+global scalingProjection
+scalingProjection = -1 ;
+
+[matUs, loadFactorsMat] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
+controlDispsNRAL_Jirasek_Green =  -matUs(6*3+5,:) ;
+loadFactorsNRAL_Jirasek_Green  =  loadFactorsMat(:,2) ;
+
+figure
+plot( controlDispsNRAL_Jirasek_Green, loadFactorsNRAL_Jirasek_Green, 'linewidth', 1.5)
+labx = xlabel('Displacement w(t)');
+laby = ylabel('\lambda(t)') ;
+legend('\fontsize{12} NRAL-Jirasek-Green','Location','southeast');
