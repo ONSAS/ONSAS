cb8404e3c444902b5aa17380ee74de064dac33ca
Sergio A. Merlino Chiozza <ing.merlino@gmail.com>
2023-11-24 19:05:01 -0300

    +

--- src/elements/frame/FramePlastic.m
+++ src/elements/frame/FramePlastic.m
@@ -55,13 +55,11 @@ function [ fs, ks, finteLocalCoor ] = linearStiffMatBeam3D(elemCoords, elemCross
 
   uvector = [u1; u2] ;
   dvector = [uvector',vvector',thetavector'] ;
-
-  % Euler–Bernoulli element with embedded discontinuity
  
   %{
 
   %}
-  
+
   % bending XY
   if     elemReleases(3) == 0 && elemReleases(4) == 0
     KbendXY = E * Iz / l^3 * kBendNoRelease ;
