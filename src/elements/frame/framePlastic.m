bb76c42a3e6015b8423303a1f8a30f3005a7ab6d
Sergio A. Merlino Chiozza <ing.merlino@gmail.com>
2023-12-26 21:13:15 -0300

    +

--- src/elements/frame/framePlastic.m
+++ src/elements/frame/framePlastic.m
@@ -34,24 +34,6 @@ function [ kpn1, xin11, xin21, alfan1, xd , ft, kt, finteLocalCoor] = framePlast
 
   [A, J, Iy, Iz] = crossSectionProps ( elemCrossSecParams, density ) ;
 
-  % numerical example
-  % cantilever beam of rectangular cross-section loaded with a vertical force at the free end
-  
-  % Mc, My, Mu / from the moment-curvature diagram
-  % kh1, kh2   / hardening modules
-  % Ks         / from the moment-rotation jump diagram
-
-  l = 2.5 ;         % m
-  E = 300000000 ;   % K(N/m^2) KPa
-  EI = 77650 ;      % KNm^2
-  Iy = EI/E ;       % m^4
-  Mc = 37.9 ;       % KNm
-  My = 268 ;
-  Mu = 374 ;
-  kh1 = 29400 ;     % KNm^2
-  kh2 = 272 ;
-  Ks = -18000 ;     % KNm
-
   % /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\
 	
   % --- elem lengths and rotation matrix
