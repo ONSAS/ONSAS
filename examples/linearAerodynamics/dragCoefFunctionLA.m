8e4502b94a8a1796d63bec3c50c143e4c01edbf0
mvanzulli <mcvanzulli@gmail.com>
2025-03-08 13:15:24 +0100

    Fix other metrics (#747)

--- examples/linearAerodynamics/dragCoefFunctionLA.m
+++ examples/linearAerodynamics/dragCoefFunctionLA.m
@@ -18,7 +18,7 @@
 function C_d = dragCoefFunctionLA(betaRel, Re)
   % Aeroelastic stability of a 3DOF system based on quasi-steady theory with reference to inertial coupling
   % Emulation of drag extrated from the reference above
-  if 0 < betaRel < deg2rad(210)
+  if 0 < betaRel  &&  betaRel < deg2rad(210)
     omega_drag = 0.8 * 2 * pi / deg2rad(180);
     C_d = +1.2 - 0.6 * cos(omega_drag * betaRel);
   else
