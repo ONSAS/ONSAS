bf0160cb0d90ccd17779f89e1f3e2be3fccc7d89
schaedleralmeida <schaedleralmeida@gmail.com>
2025-04-11 11:51:37 -0300

    modifications to use localShellTriangle()

--- src/elements/triangle/internalForcesLinearShellTriangle.m
+++ src/elements/triangle/internalForcesLinearShellTriangle.m
@@ -22,84 +22,30 @@
 function [fs, ks, fintLocCoord] = internalForcesLinearShellTriangle(elemCoords, elemDisps, modelName, modelParams, thickness)
 
   % material and geometric parameters
-  E = modelParams(1);
-  nu = modelParams(2);
+  young_modulus = modelParams(1);
+  poisson_ratio = modelParams(2);
   h = thickness;
 
-  p1 = elemCoords(1:3);
-  p2 = elemCoords(4:6);
-  p3 = elemCoords(7:9);
+  elemCoords = elemCoords';
 
-  elemDisps_sortT = switchToTypeIndexing(elemDisps);
+  r1g = elemCoords(1:3);
+  r2g = elemCoords(4:6);
+  r3g = elemCoords(7:9);
 
-  [T, x02, x03, y03] = edgeLocalAxisShellTriangle(p1, p2, p3);
+  Ug = switchToTypeIndexing(elemDisps);
+
+  [T, x02, x03, y03] = edgeLocalAxisShellTriangle(r1g, r2g, r3g);
   Te = blkdiag(T, T, T, T, T, T);
 
-  area = x02 * y03 / 2;
+  Ul = Te * Ug;
 
-  % membrane stiffness
-  aux1 = h *  E / (1 - nu^2);
-  aux2 = nu * aux1;
-  Dm = [[aux1, aux2, 0]; [aux2, aux1, 0]; [0, 0, aux1 * (1 - nu) / 2]];
+  [Kl, fintLocCoord] = localShellTriangle(x02, x03, y03, young_modulus, poisson_ratio, h, Ul);
 
-  Bm = cstB(x02, x03, y03);
-  Km = area * Bm' * Dm * Bm;
+  Kg = Te' * Kl * Te;
+  fg = Kg * Ug;
 
-  % bending stiffness
-  aux1 = E * h^3 / (12 * (1 - nu^2));
-  aux2 = nu * aux1;
-  Db = [[aux1, aux2, 0]; [aux2, aux1, 0]; [0, 0, aux1 * (1 - nu) / 2]];
+  ks = {switchToNodalIndexing(Kg)};
+  fs = {switchToNodalIndexing(fg)};
 
-  int_point = [[1 ./ 6. 1 ./ 6.]; [2 ./ 3., 1 ./ 6.]; [1 ./ 6., 2 ./ 3.]];
-
-  im = [1, 2, 7, 8, 13, 14];
-  ib = [3, 4, 5, 9, 10, 11, 15, 16, 17];
-
-  fintLocCoord = zeros(1, 3);
-
-  dispTe = Te * elemDisps_sortT;
-
-  % TODO: temporal FIXX generalize!
-  TM = T(1:2, 1:2);
-  % ------------------------------
-
-  Kb = zeros(9, 9);
-  wgt = area / 3.0;
-  for ipt = 1:3
-    psi = int_point(ipt, 1);
-    eta = int_point(ipt, 2);
-    Bb = dktB(psi, eta, x02, x03, y03);
-
-    Kb = Kb + wgt * Bb' * Db * Bb;
-
-    fint_ip = Db * Bb * dispTe(ib);
-    Mmat    = (TM') * [fint_ip(1) fint_ip(3); fint_ip(3) fint_ip(2)] * TM;
-    fint_ip = [Mmat(1, 1) Mmat(2, 2) Mmat(1, 2)];
-    fintLocCoord = fintLocCoord + fint_ip * wgt / area;
-  end
-
-  % assembling the stiffness matrix of the shell element in local coordinates
-  Ke = zeros(18, 18);
-
-  Ke(im, im) = Km;
-  Ke(ib, ib) = Kb;
-
-  k_dr = min(min(abs(Kb))) * 1.0e-4;
-  Ke(6,   6) = k_dr;
-  Ke(12, 12) = k_dr;
-  Ke(18, 18) = k_dr;
-
-  % calculating the stiffness matrix of the shell element in global coordinates
-  Ke = Te' * Ke * Te;
-
-  % shifting lines and coluns to onsas convention of dofs order
-  aux_r = [1, 4, 2, 5, 3, 6];
-  aux_onsas = [aux_r, aux_r + 6, aux_r + 12];
-
-  K = Ke(aux_onsas, aux_onsas);
-  f = K * elemDisps;
-
-  ks = {K};
-  fs = {f};
 end
 
