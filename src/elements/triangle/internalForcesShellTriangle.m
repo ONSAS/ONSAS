640e407ec8f24d784dc83b031d3d4d3afc17b7f2
schaedleralmeida <schaedleralmeida@gmail.com>
2025-04-11 11:16:24 -0300

    cstB() was moved to cstB.m

--- src/elements/triangle/internalForcesShellTriangle.m
+++ src/elements/triangle/internalForcesShellTriangle.m
@@ -197,29 +197,6 @@ function [T, x02, x03, y03] = edgeLocalAxisShellTriangle(p1, p2, p3)
   y03 = dot(u_yl, p13);
 end
 
-function [B] = cstB(x02, x03, y03)
-  % calculate the strain-displacement matrix for the constant stress triangular element (CST)
-  % x02, x03 and y03 are the local coordinates of the nodes 2 and 3
-
-  area02 = x02 * y03;
-
-  % ( with y01 = y02 = x01 = 0)
-  % bi = (yj - yk )/2A
-  b1 = -y03 / area02;
-  b2 =   y03 / area02;
-  b3 =  0;
-
-  % ci = (xk - xj)/2A
-  c1 = (x03 - x02) / area02;
-  c2 = -x03 / area02;
-  c3 = x02 / area02;
-
-  B = [[b1,  0, b2,  0, b3,  0]
-       [0, c1,  0, c2,  0, c3]
-       [c1, b1, c2, b2, c3, b3]];
-
-end
-
 function [R] = globalRotationMatrix(q)
   % Eq. (35) of 10.1016/j.cma.2006.10.006
   q1 = q(1);
