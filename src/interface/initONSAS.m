b50a8b7045c8a67cc66bf7905e781b88655cb309
jorgepz <42485529+jorgepz@users.noreply.github.com>
2025-11-18 23:58:32 -0300

    fixing

--- src/interface/initONSAS.m
+++ src/interface/initONSAS.m
@@ -18,198 +18,175 @@
 
 function [modelCurrSol, modelProperties, BCsData] = initONSAS(materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams)
 
-  % checks if the fields defined are correct or not
-  checkONSASFields(materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams);
 
-  % md set defaults
-  [materials, elements, boundaryConds, analysisSettings, otherParams] = setDefaults(materials, elements, boundaryConds, analysisSettings, otherParams);
+global TZERO
 
-  % md sets the current version and welcomes user
-  ONSASversion = '0.4.0';
-  welcomeMessage(ONSASversion, otherParams);
+% checks if the fields defined are correct or not
+checkONSASFields(materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams )
 
-  % creates outputdir in current location
-  createOutputDir(otherParams);
+%md set defaults
+[ materials, elements, boundaryConds, analysisSettings, otherParams ] = setDefaults( materials, elements, boundaryConds, analysisSettings, otherParams ) ;
 
-  % =================================================================
-  % md process boundary conds information and construct BCsData struct
-  [Conec, Nodes, factorLoadsFextCell, loadFactorsFuncCell, diriDofs, neumDofs, KS, userLoadsFilename] ...
-    = boundaryCondsProcessing(mesh, materials, elements, boundaryConds, analysisSettings);
+%md sets the current version and welcomes user
+ONSASversion = '0.3.0'  ; welcome_message( ONSASversion, otherParams );
 
-  BCsData = constructBCsData(factorLoadsFextCell, loadFactorsFuncCell, neumDofs, KS, userLoadsFilename);
-  % =================================================================
+% creates outputdir in current location
+create_outputDir( otherParams )
 
-  nTimes = round(analysisSettings.finalTime / analysisSettings.deltaT) + 1; % number of times (including t=0)
-  % if length( otherParams.plotParamsVector ) > 1
-  %   nplots = min( [ nTimes otherParams.plotParamsVector(2) ] ) ;
-  % else
-  %   % default value: all
-  nplots = nTimes;
-  % end
-  timesPlotsVec = round(linspace(1, nTimes, nplots)');
+% =================================================================
+%md process boundary conds information and construct BCsData struct
+[ Conec, Nodes, factorLoadsFextCell, loadFactorsFuncCell, diriDofs, neumDofs, KS, userLoadsFilename ] ...
+  = boundaryCondsProcessing( mesh, materials, elements, boundaryConds, analysisSettings ) ;
 
-  % =================================================================
-  % md construct modelProperties struct
-  modelProperties = constructModelProperties(Nodes, Conec, materials, elements, analysisSettings, otherParams, timesPlotsVec);
-  % =================================================================
+BCsData = construct_BCsData( factorLoadsFextCell, loadFactorsFuncCell, neumDofs, KS, userLoadsFilename );
+% =================================================================
 
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  % TEMPORARY
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  global spitMatrices
-  if spitMatrices == true
-    save('-mat', 'output/loads.mat', 'factorLoadsFextCell');
+
+nTimes = round( analysisSettings.finalTime / analysisSettings.deltaT ) + 1 ; % number of times (including t=0)
+% if length( otherParams.plotParamsVector ) > 1
+%   nplots = min( [ nTimes otherParams.plotParamsVector(2) ] ) ;
+% else
+%   % default value: all
+   nplots = nTimes ;
+% end
+timesPlotsVec = round( linspace( 1, nTimes, nplots )' ) ;
+
+% =================================================================
+%md construct modelProperties struct
+modelProperties = construct_modelProperties( Nodes, Conec, materials, elements, analysisSettings, otherParams, timesPlotsVec ) ;
+% =================================================================
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+global spitMatrices
+if spitMatrices == true
+  save('-mat', 'output/loads.mat', 'factorLoadsFextCell' );
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% =================================================================
+%md process initial conds and construct modelSol struct
+currTime  = 0 ; timeIndex = 1 ; 
+
+timeStepIters    = 0 ; timeStepStopCrit = 0 ;
+
+% process initial conditions
+nNodes = size( mesh.nodesCoords, 1 ) ;
+[ U, Udot, Udotdot ] = initialCondsProcessing( initialConds, nNodes ) ;
+
+convDeltau   = zeros( size(U) ) ; 
+
+TZERO = zeros(1, size(Conec,1)) ;
+
+%~ previousStateCell = zeros( size(Conec,1), 3 ) ; % assumed only for trusses: scalar per element
+previousStateCell = zeros( size(Conec,1), 11 ) ;  % matrix of plastic parameters for all elements
+
+% comput internal forces and stresses
+[~, Stress, ~, localInternalForces, strain_vec, acum_plas_strain_vec ] = assembler ( modelProperties.Conec, modelProperties.elements, modelProperties.Nodes, modelProperties.materials, BCsData.KS, U, Udot, Udotdot, modelProperties.analysisSettings, [ 0 1 0 1 ], modelProperties.nodalDispDamping, currTime, previousStateCell ) ;
+
+[FextG, currLoadFactorsVals ]  = computeFext( modelProperties, BCsData, 0, length(U), [] , {U, Udot, Udotdot})  ;
+
+nextTime = currTime + analysisSettings.deltaT ;
+
+%md call assembler
+[ systemDeltauMatrix, systemDeltauRHS, ~, ~, ~, ~ , modelProperties.exportFirstMatrices  ] = system_assembler( modelProperties, BCsData, U, Udot, Udotdot, U, Udot, Udotdot, nextTime, [], previousStateCell ) ;
+
+modelCurrSol = construct_modelSol( timeIndex, currTime, U, Udot, Udotdot, Stress, convDeltau, ...
+    currLoadFactorsVals, systemDeltauMatrix, systemDeltauRHS, timeStepStopCrit, timeStepIters, localInternalForces, previousStateCell ) ;
+% =================================================================
+
+
+%md prints headers for solver output file
+printSolverOutput( otherParams.outputDir, otherParams.problemName, 0                  , [] ) ;
+printSolverOutput( otherParams.outputDir, otherParams.problemName, [ 2 timeIndex currTime 0 0 ], [] ) ;
+
+%md writes vtk file
+if strcmp( modelProperties.plots_format, 'vtk' )
+  vtkMainWriter( modelCurrSol, modelProperties )
+end
+
+if exist( 'controlDofs') ==0
+  controlDofs = [] ;
+  controlDofsAndFactors = [] ;
+end
+
+if length( controlDofs ) > 0
+  controlDofsAndFactors = zeros( size( controlDofs,1 ) , 2 ) ;
+
+  % control dof info
+  for i=1:size(controlDofs,1)
+    aux                = nodes2dofs( controlDofs(i,1), 6 ) ;
+    controlDofsAndFactors(i,:) = [ aux( controlDofs(i, 2) ) controlDofs(i,3) ] ;
   end
+end
 
-  % Initialize VIV-related vectors if VIV is enabled
-  if any(analysisSettings.crossFlowVIVBool) || any(analysisSettings.inLineVIVBool)
-    numElements = size(modelProperties.Conec, 1);
-    dofsPerElement = 2;
-    numTimeSteps = round(analysisSettings.finalTime / analysisSettings.deltaT) + 1;
-    if analysisSettings.crossFlowVIVBool
-      global qvect
-      qvect = zeros(numElements * dofsPerElement, numTimeSteps);
-      qvect(:, 1) = initialConds.Q0;
-    end
-    if analysisSettings.inLineVIVBool
-      global pvect
-      pvect = zeros(numElements * dofsPerElement, numTimeSteps);
-      pvect(:, 1) = initialConds.P0;
-    end
-  end
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% =========================================
+% function for creation of output directory
+% -----------------------------------------
+function create_outputDir( otherParams )
 
-  % =================================================================
-  % md process initial conds and construct modelSol struct
-  currTime  = 0;
-  timeIndex = 1;
+outputDir = otherParams.outputDir ;
 
-  timeStepIters    = 0;
-  timeStepStopCrit = 0;
-
-  % process initial conditions
-  nNodes = size(mesh.nodesCoords, 1);
-  [U, Udot, Udotdot] = initialCondsProcessing(initialConds, nNodes);
-
-  convDeltau   = zeros(size(U));
-
-  % ~ previousStateCell = zeros( size(Conec,1), 3 ) ; % assumed only for trusses: scalar per element
-  previousStateCell = cell(size(Conec, 1), 3);
-  previousStateCell(:, 1) = {zeros(1, 3)};
-  previousStateCell(:, 2) = {zeros(1, 3)};
-  previousStateCell(:, 3) = {0};
-
-  % ==========================================================================================
-  % ==========================================================================================
-  % Global rotation matrices for triangle elements
-  rotMatCell = cell(size(U, 1) / 6);
-  % Esto se tiene que mejorar, se asume que no hay rotaciones iniciales
-  rotMatCell(:) = {eye(3)};
-  % ==========================================================================================
-  % ==========================================================================================
-
-  % comput internal forces and stresses
-  [~, Stress, ~, localInternalForces, strain_vec, acum_plas_strain_vec] = assembler(modelProperties.Conec, modelProperties.elements, modelProperties.Nodes, modelProperties.materials, BCsData.KS, U, Udot, Udotdot, modelProperties.analysisSettings, [0 1 0 1], modelProperties.nodalDispDamping, currTime, previousStateCell, rotMatCell);
-
-  [FextG, currLoadFactorsVals]  = computeFext(modelProperties, BCsData, 0, length(U), [], {U, Udot, Udotdot});
-
-  nextTime = currTime + analysisSettings.deltaT;
-
-  % md call assembler
-  [systemDeltauMatrix, systemDeltauRHS, ~, ~, ~, ~, modelProperties.exportFirstMatrices] = systemAssembler(modelProperties, BCsData, U, Udot, Udotdot, U, Udot, Udotdot, nextTime, [], previousStateCell, rotMatCell);
-
-  modelCurrSol = constructModelSol(timeIndex, currTime, U, Udot, Udotdot, Stress, convDeltau, ...
-                                   currLoadFactorsVals, systemDeltauMatrix, systemDeltauRHS, timeStepStopCrit, timeStepIters, localInternalForces, previousStateCell, rotMatCell);
-  % =================================================================
-
-  % md prints headers for solver output file
-  printSolverOutput(otherParams.outputDir, otherParams.problemName, 0, []);
-  printSolverOutput(otherParams.outputDir, otherParams.problemName, [2 timeIndex currTime 0 0], []);
-
-  % md writes vtk file
-  if strcmp(modelProperties.plots_format, 'vtk')
-    vtkMainWriter(modelCurrSol, modelProperties);
-  end
-
-  if exist('controlDofs') == 0
-    controlDofs = [];
-    controlDofsAndFactors = [];
-  end
-
-  if length(controlDofs) > 0
-    controlDofsAndFactors = zeros(size(controlDofs, 1), 2);
-
-    % control dof info
-    for i = 1:size(controlDofs, 1)
-      aux                = nodes2dofs(controlDofs(i, 1), 6);
-      controlDofsAndFactors(i, :) = [aux(controlDofs(i, 2)) controlDofs(i, 3)];
-    end
-  end
-
-  % =========================================
-  % function for creation of output directory
-  % -----------------------------------------
-function createOutputDir(otherParams)
-
-  outputDir = otherParams.outputDir;
-
-  if exist('./output/') ~= 7
-    if otherParams.screenOutputBool
-      fprintf('  - Creating directory ./output/ ...');
-    end
-
-    mkdir('./', './output/');
-
-    if otherParams.screenOutputBool
-      fprintf(' done. \n');
-    end
-  end
-
-  % -----------------
-  if exist(outputDir) == 7 % problemName is a directory
-    % the content is erased
-    if otherParams.screenOutputBool
-      fprintf(['|  - Cleaning output directory ...']);
-    end
-    if isThisOctave
-      confirm_recursive_rmdir(0);
-    end
-
-    % delete
-    [aux, msg] = rmdir(outputDir, 's');
-
-    % create empty
-    mkdir(outputDir);
-
-  elseif exist(['./' otherParams.problemName '/']) ~= 7 % problemName is not a directory
-    % it is created
-    if otherParams.screenOutputBool
-      fprintf(['|  - Creating output directory ...']);
-    end
-    mkdir(outputDir);
-  end
+if exist( './output/' ) ~= 7
   if otherParams.screenOutputBool
-    fprintf(' done. \n');
+    fprintf( '  - Creating directory ./output/ ...' );
   end
 
-  % =========================================
-  % function for welcome message
-  % -----------------------------------------
-function welcomeMessage(ONSASversion, otherParams)
+  mkdir('./', './output/' );
+
   if otherParams.screenOutputBool
-    fprintf(['\n' ...
-             '|=================================================|\n' ...
-             '|         _ _             _ _     _ _     _ _     |\n' ...
-             '|       /    /  /|   /  /       /    /  /         |\n' ...
-             '|      /    /  / |  /  /_ _    /_ _ /  /_ _       |\n' ...
-             '|     /    /  /  | /       /  /    /       /      |\n' ...
-             '|    /_ _ /  /   |/   _ _ /  /    /   _ _ /       |\n' ...
-             '|                                                 |\n' ...
-             '|-------------------------------------------------|\n']);
-    fprintf(['| Welcome to ONSAS v' ONSASversion '.                        |\n' ...
-             '| This program comes with ABSOLUTELY NO WARRANTY. |\n' ...
-             '| Please read the COPYING.txt and README.md files |\n' ...
-             '|-------------------------------------------------|\n']);
-    fprintf(['| Solving problem:  ' otherParams.problemName '\n']);
+    fprintf( ' done. \n' );
   end
+end
+
+% -----------------
+if exist( outputDir ) == 7 % problemName is a directory
+  % the content is erased
+  if otherParams.screenOutputBool
+    fprintf( ['|  - Cleaning output directory ...'] ) ;
+  end
+  if isThisOctave
+    confirm_recursive_rmdir(0)
+  end
+
+  % delete
+  [aux, msg] = rmdir( outputDir ,'s') ;
+
+  % create empty
+  mkdir( outputDir );
+
+elseif exist( ['./' otherParams.problemName '/' ] ) ~= 7 % problemName is not a directory
+  % it is created
+  if otherParams.screenOutputBool
+    fprintf( ['|  - Creating output directory ...'] ) ;
+  end
+  mkdir( outputDir );
+end
+if otherParams.screenOutputBool
+  fprintf( ' done. \n' );
+end
+
+
+% =========================================
+% function for welcome message
+% -----------------------------------------
+function welcome_message( ONSASversion, otherParams )
+if otherParams.screenOutputBool
+  fprintf([ '\n' ...
+            '|=================================================|\n' ...
+            '|         _ _             _ _     _ _     _ _     |\n' ...
+            '|       /    /  /|   /  /       /    /  /         |\n' ...
+            '|      /    /  / |  /  /_ _    /_ _ /  /_ _       |\n' ...
+            '|     /    /  /  | /       /  /    /       /      |\n' ...
+            '|    /_ _ /  /   |/   _ _ /  /    /   _ _ /       |\n' ...
+            '|                                                 |\n' ...
+            '|-------------------------------------------------|\n' ] );
+  fprintf([ '| Welcome to ONSAS v' ONSASversion '.                        |\n' ...
+            '| This program comes with ABSOLUTELY NO WARRANTY. |\n' ...
+            '| Please read the COPYING.txt and README.md files |\n' ...
+            '|-------------------------------------------------|\n'] ) ;
+  fprintf( ['| Solving problem:  ' otherParams.problemName '\n' ] ) ;
+end
--- src/interface/solveONSAS.m
+++ src/interface/solveONSAS.m
@@ -21,136 +21,144 @@
 %
 function [matUs, loadFactorsMat, modelSolutions] = solveONSAS(modelCurrSol, modelProperties, BCsData)
 
-  % initialize structures to store solutions
-  matUs          = modelCurrSol.U;
-  loadFactorsMat = modelCurrSol.currLoadFactorsVals;
-  matUdots       = modelCurrSol.Udot;
-  cellFint       = {modelCurrSol.localInternalForces};
+global historic_parameters
+global Timex
 
-  modelSolutions = {modelCurrSol};
-  cellStress     = {modelCurrSol.Stress}; % cell with a matrix with all elements stresses at each time
+% initialize structures to store solutions
+matUs          = modelCurrSol.U                   ;
+loadFactorsMat = modelCurrSol.currLoadFactorsVals ;
+matUdots       = modelCurrSol.Udot                ;
+cellFint       = {modelCurrSol.localInternalForces} ; 
 
-  % Incremental time analysis
-  % sets stopping boolean to false
-  finalTimeReachedBoolean = false;
-  % and starts the iteration
-  fprintf('|                                                 |\n');
-  fprintf('| Analysis progress:   |0       50       100| %%   |\n');
-  fprintf('|                      |');
+modelSolutions = {modelCurrSol};
+cellStress     = {modelCurrSol.Stress}  ; % cell with a matrix with all elements stresses at each time
 
-  % iteration variables
-  iterations_average = 0;
-  iterations_maximum = 0;
-  iterations_strop_crit_vec = [0 0 0];
+% Incremental time analysis
+% sets stopping boolean to false
+finalTimeReachedBoolean = false ;
+% and starts the iteration
+fprintf('|                                                 |\n')
+fprintf('| Analysis progress:   |0       50       100| %%   |\n')
+fprintf('|                      |')
 
-  % progress bar variables
-  plotted_bars = 0;
-  aux_time = cputime();
+% iteration variables
+iterations_average = 0 ;  iterations_maximum = 0 ;
+iterations_strop_crit_vec = [ 0 0 0 ] ;
 
-  while finalTimeReachedBoolean == false
-    plotted_bars = progressBarPlot(modelCurrSol, modelProperties, plotted_bars);
+% progress bar variables
+plotted_bars = 0 ; aux_time = cputime() ;
 
-    % compute the model state at next time
-    modelNextSol = timeStepIteration(modelCurrSol, modelProperties, BCsData);
+while finalTimeReachedBoolean == false
 
-    % iterations average
-    iterations_average = ...
-      (iterations_average * (modelNextSol.timeIndex - 2) + modelNextSol.timeStepIters) / ...
-      (modelNextSol.timeIndex - 1);
+  % disp(' ====================================================') ;
+  % disp(' /\  /\  /\  /\  /\  NEW TIME STEP /\  /\  /\  /\  /\') ;
+  plotted_bars = progressBarPlot( modelCurrSol, modelProperties, plotted_bars) ;
 
-    % iterations max
-    iterations_maximum = max(iterations_maximum, modelNextSol.timeStepIters);
-    iterations_strop_crit_vec(modelNextSol.timeStepStopCrit) = ...
-      iterations_strop_crit_vec(modelNextSol.timeStepStopCrit) + 1;
+  Timex = modelCurrSol.currTime ;
 
-    % check if final time was reached
-    finalTimeReachedBoolean = (modelNextSol.currTime - modelProperties.analysisSettings.finalTime)  >= ...
-                              (-(modelProperties.analysisSettings.finalTime) * 1e-8);
+  % compute the model state at next time
+  modelNextSol = timeStepIteration( modelCurrSol, modelProperties, BCsData ) ;
 
-    % store results and update structs
-    modelCurrSol    =   modelNextSol;
-    matUs           = [matUs          modelCurrSol.U];
-    loadFactorsMat  = [loadFactorsMat; modelCurrSol.currLoadFactorsVals];
-    cellFint{end + 1}   = modelCurrSol.localInternalForces;
-    cellStress{end + 1} = modelCurrSol.Stress;
+  historic_parameters = [ historic_parameters ;  modelNextSol.previousStateCell ] ;
 
-    modelSolutions{end + 1} = modelCurrSol;
+  % iterations average
+  iterations_average = ...
+    (   iterations_average* (modelNextSol.timeIndex-2) + modelNextSol.timeStepIters ) ...
+    / ( modelNextSol.timeIndex-1) ;
 
-    % generate vtk file for the new state
-    if strcmp(modelProperties.plots_format, 'vtk')
-      vtkMainWriter(modelCurrSol, modelProperties);
-    end % if vtk output format
+  % iterations max
+  iterations_maximum = max( iterations_maximum, modelNextSol.timeStepIters ) ;
+  iterations_strop_crit_vec( modelNextSol.timeStepStopCrit ) = ...
+    iterations_strop_crit_vec( modelNextSol.timeStepStopCrit ) + 1 ;
 
-  end % while time
-  time_solve = cputime() - aux_time;
-  fprintf('|     |\n');
+  % check if final time was reached
+  finalTimeReachedBoolean = ( modelNextSol.currTime - modelProperties.analysisSettings.finalTime ) ...
+                        >= ( -(modelProperties.analysisSettings.finalTime) * 1e-8 ) ;
+ 
+  % store results and update structs
+  modelCurrSol   	=  	modelNextSol ;
+  matUs          	= [ matUs          modelCurrSol.U                     ] ;
+  loadFactorsMat 	= [ loadFactorsMat ; modelCurrSol.currLoadFactorsVals ] ;	
+	cellFint{end+1}  	= modelCurrSol.localInternalForces ;
+	cellStress{end+1}	= modelCurrSol.Stress ;
 
-  % ---- print iteration statistics -----
-  fprintf('| Time: %6.1f sec                                |\n', time_solve);
-  fprintf('|                                                 |\n');
-  fprintf('| Iters:  avg  max | Stop by: force  disp  iters  |\n');
-  fprintf('|        %4.1f  %3i |          %5i %5i  %5i  |\n', ...
-          iterations_average, iterations_maximum, iterations_strop_crit_vec(1), ...
-          iterations_strop_crit_vec(2), iterations_strop_crit_vec(3));
-  % -------------------------------------
+  modelSolutions{end+1} = modelCurrSol ;
 
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  %%%%% Experimental Modal Analysis Block %%%%%%
-  if modelProperties.analysisSettings.modalAnalysisBoolean
-    modal_output_folder = [pwd filesep 'output'];
-    addpath(genpath(modal_output_folder));
-    load([modal_output_folder filesep 'matrices.mat']);
+  % generate vtk file for the new state
+  if strcmp( modelProperties.plots_format, 'vtk' )
+    vtkMainWriter( modelCurrSol, modelProperties );
+  end % if vtk output format
 
-    Kred = KT(BCsData.neumDofs, BCsData.neumDofs);
-    Mred = massMat(BCsData.neumDofs, BCsData.neumDofs);
-    % Mred = Mred + speye(size(Mred,1));
-    numModes = 5;
+end %while time
+time_solve = cputime() - aux_time ;
+fprintf('|     |\n')
 
-    sparse_analysis = false;
-    if sparse_analysis % sparse analysis
-      Mred = Mred + speye(size(Mred, 1));
-      [PHI, OMEGA] = eigs(Mred^(-1) * Kred, numModes, 'sm');
-    else % dense analysis
-      [PHI, OMEGA] = eig(full(Kred), full(Mred));
-      numer_modes = fliplr(PHI);
+% ---- print iteration statistics -----
+fprintf('| Time: %6.1f sec                                |\n',time_solve)
+fprintf('|                                                 |\n')
+fprintf('| Iters:  avg  max | Stop by: force  disp  iters  |\n')
+fprintf('|        %4.1f  %3i |          %5i %5i  %5i  |\n', ...
+  iterations_average, iterations_maximum, iterations_strop_crit_vec(1), ...
+  iterations_strop_crit_vec(2), iterations_strop_crit_vec(3) )
+% -------------------------------------
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% Experimental Modal Analysis Block %%%%%%
+if modelProperties.analysisSettings.modalAnalysisBoolean
+  modal_output_folder = [ pwd filesep 'output'] ;  
+  addpath( genpath( modal_output_folder ) ) ;
+  load( [ modal_output_folder filesep 'matrices.mat'] ) ;
+
+  Kred = KT(BCsData.neumDofs,BCsData.neumDofs);
+  Mred = massMat(BCsData.neumDofs,BCsData.neumDofs);
+  %Mred = Mred + speye(size(Mred,1));
+  numModes = 5 ;
+
+  sparse_analysis = false ;
+  if sparse_analysis % sparse analysis
+    Mred = Mred + speye(size(Mred,1));
+    [PHI, OMEGA] = eigs(Mred^(-1)*Kred,numModes,'sm');
+  else % dense analysis
+    [PHI, OMEGA] = eig( full(Kred), full(Mred) ) ;
+    numer_modes = fliplr(PHI);
+  end
+
+  s = size(OMEGA) ;
+  index = 1:s(1)+1:s(1)*s(2) ;
+  [elem_diag, ind] = sort(diag(OMEGA)) ;
+  OMEGA(index) = elem_diag ;
+  PHI = PHI(:,ind) ;
+
+  modelPropertiesModal = modelProperties ;
+  modelPropertiesModal.plots_deltaTs_separation = 1 ;
+  modelPropertiesModal.analysisSettings.deltaT  = 1 ;
+
+  modelCurrSolModal   = modelCurrSol    ;
+  modelCurrSolModal.U = zeros( size(modelCurrSol.U, 1) , 1 )    ;
+
+  num_modal_times = 15 ;
+  for i = 1:numModes
+    fprintf(' generating mode %2i vtk\n', i) ;
+    for j = 1:num_modal_times
+      modelCurrSolModal.currTime = j ;
+      modelPropertiesModal.problemName = [ modelProperties.problemName sprintf('_mode_%02i_', i ) ] ;
+      modelCurrSolModal.U( BCsData.neumDofs ) = sin(2*pi*j/num_modal_times) * numer_modes(:,i)  ;
+      vtkMainWriter( modelCurrSolModal, modelPropertiesModal ) ;
     end
+  end
+  if isThisOctave
+    save('-binary','Modal.mat','PHI','OMEGA')
+  end
+  fprintf(' MODAL ANALYSIS DONE. Setting modalAnalysisBoolean to false.\n')
+  modelProperties.analysisSettings.modalAnalysisBoolean = false ;
+end %endif
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    s = size(OMEGA);
-    index = 1:s(1) + 1:s(1) * s(2);
-    [elem_diag, ind] = sort(diag(OMEGA));
-    OMEGA(index) = elem_diag;
-    PHI = PHI(:, ind);
 
-    modelPropertiesModal = modelProperties;
-    modelPropertiesModal.plots_deltaTs_separation = 1;
-    modelPropertiesModal.analysisSettings.deltaT  = 1;
+function plotted_bars = progressBarPlot( modelCurrSol, modelProperties, plotted_bars)
 
-    modelCurrSolModal   = modelCurrSol;
-    modelCurrSolModal.U = zeros(size(modelCurrSol.U, 1), 1);
-
-    num_modal_times = 15;
-    for i = 1:numModes
-      fprintf(' generating mode %2i vtk\n', i);
-      for j = 1:num_modal_times
-        modelCurrSolModal.currTime = j;
-        modelPropertiesModal.problemName = [modelProperties.problemName sprintf('_mode_%02i_', i)];
-        modelCurrSolModal.U(BCsData.neumDofs) = sin(2 * pi * j / num_modal_times) * numer_modes(:, i);
-        vtkMainWriter(modelCurrSolModal, modelPropertiesModal);
-      end
-    end
-    if isThisOctave
-      save('-binary', 'Modal.mat', 'PHI', 'OMEGA');
-    end
-    fprintf(' MODAL ANALYSIS DONE. Setting modalAnalysisBoolean to false.\n');
-    modelProperties.analysisSettings.modalAnalysisBoolean = false;
-  end % endif
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function plotted_bars = progressBarPlot(modelCurrSol, modelProperties, plotted_bars)
-
-  percent_time = round((modelCurrSol.timeIndex * modelProperties.analysisSettings.deltaT) / ...
-                       modelProperties.analysisSettings.finalTime * 20);
-  while plotted_bars < percent_time
-    fprintf('=');
-    plotted_bars = plotted_bars + 1;
+  percent_time = round( (modelCurrSol.timeIndex * modelProperties.analysisSettings.deltaT) ...
+                       / modelProperties.analysisSettings.finalTime * 20 ) ;
+  while plotted_bars < percent_time,
+    fprintf('='); plotted_bars = plotted_bars +1 ;
   end
